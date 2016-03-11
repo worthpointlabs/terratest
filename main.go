b8e77fed0ebbfaeab0230ec189db0f73dcc5621e
Josh Padnick <josh.padnick@gmail.com>
2016-03-11 16:41:43 -0700

    Output S3 info on Terraform Destroy failure.

--- main.go
+++ main.go
@@ -87,10 +87,11 @@ func TerraformDestroyHelper(testName string, templatePath string, vars map[strin
 	if err != nil {
 		logger.Printf(`Failed to terraform destroy.
 ** WARNING ** Terraform destroy has failed which means you must manually delete any resources created by the "terraform apply" run.
-Terraform Template Path: %s
 Test Name: %s
-AWS region: <scroll up to see it>
+Terraform Template Path: %s
+AWS Region: <scroll up to see it>
+Remote State Location: s3://%s/%s
 Official Error Message: %s
-`, templatePath, testName, err.Error())
+`, templatePath, testName, remoteStateS3BucketName, remoteStateS3ObjectName, err.Error())
 	}
 }
