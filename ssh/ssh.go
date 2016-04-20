f4f7b0769e5cb94c05f743d3847eb63182c499bc
Yevgeniy Brikman <brikis98@gmail.com>
2016-04-20 15:13:15 +0200

    Use unique names for Key Pair files

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"io/ioutil"
 	"os"
+	"fmt"
+"github.com/gruntwork-io/terratest/util"
 )
 
 type Host struct {
@@ -17,10 +19,12 @@ type Host struct {
 }
 
 func CheckSshConnection(host Host, logger *log.Logger) error {
-	defer cleanupKeyPairFile(host.SshKeyPair, logger)
-	writeKeyPairFile(host.SshKeyPair, logger)
+	keyPairWithUniqueName := createKeyPairCopyWithUniqueName(*host.SshKeyPair)
 
-	sshErr := shell.RunCommand(shell.Command{Command: "ssh", Args: []string{"-i", host.SshKeyPair.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", host.SshUserName + "@" + host.Hostname, "'exit'"}}, logger)
+	defer cleanupKeyPairFile(keyPairWithUniqueName, logger)
+	writeKeyPairFile(keyPairWithUniqueName, logger)
+
+	sshErr := shell.RunCommand(shell.Command{Command: "ssh", Args: []string{"-i", keyPairWithUniqueName.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", host.SshUserName + "@" + host.Hostname, "'exit'"}}, logger)
 
 	exitCode, err := shell.GetExitCodeForRunCommandError(sshErr)
 
@@ -39,16 +43,19 @@ func CheckSshConnection(host Host, logger *log.Logger) error {
 // publicHost (which is addressable from the Internet) and then executes "command" on privateHost and returns its output.
 // It is useful for checking that it's possible to SSH from a Bastion Host to a private instance.
 func CheckPrivateSshConnection(publicHost Host, privateHost Host, command string, logger *log.Logger) (string, error) {
-	defer cleanupKeyPairFile(publicHost.SshKeyPair, logger)
-	writeKeyPairFile(publicHost.SshKeyPair, logger)
+	publicKeyPairWithUniqueName := createKeyPairCopyWithUniqueName(*publicHost.SshKeyPair)
+	privateKeyPairWithUniqueName := createKeyPairCopyWithUniqueName(*privateHost.SshKeyPair)
 
-	defer cleanupKeyPairFile(privateHost.SshKeyPair, logger)
-	writeKeyPairFile(privateHost.SshKeyPair, logger)
+	defer cleanupKeyPairFile(publicKeyPairWithUniqueName, logger)
+	writeKeyPairFile(publicKeyPairWithUniqueName, logger)
+
+	defer cleanupKeyPairFile(privateKeyPairWithUniqueName, logger)
+	writeKeyPairFile(privateKeyPairWithUniqueName, logger)
 
 	// We need the SSH key to be available when we SSH from the Bastion Host to the Private Host.
 	// We cannot guarantee ssh-agent will be in the test environment, so we use scp to copy the key to the bastion host file system.
 	// Start by setting permissions on the key to 0600. These permissions (read/write for file owner only) are required by ssh to access the key.
-	chmodErr := shell.RunCommand(shell.Command{Command: "chmod", Args: []string{"0600", privateHost.SshKeyPair.Name}}, logger)
+	chmodErr := shell.RunCommand(shell.Command{Command: "chmod", Args: []string{"0600", privateKeyPairWithUniqueName.Name}}, logger)
 	exitCode, err := shell.GetExitCodeForRunCommandError(chmodErr)
 	if err != nil {
 		return "", err
@@ -58,7 +65,7 @@ func CheckPrivateSshConnection(publicHost Host, privateHost Host, command string
 	}
 
 	// Upload the key to the bastion host
-	sshErr := shell.RunCommand(shell.Command{Command: "scp", Args: []string{"-p", "-i", publicHost.SshKeyPair.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", privateHost.SshKeyPair.Name, publicHost.SshUserName + "@" + publicHost.Hostname + ":key.pem"}}, logger)
+	sshErr := shell.RunCommand(shell.Command{Command: "scp", Args: []string{"-p", "-i", publicKeyPairWithUniqueName.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", privateKeyPairWithUniqueName.Name, publicHost.SshUserName + "@" + publicHost.Hostname + ":key.pem"}}, logger)
 	exitCode, err = shell.GetExitCodeForRunCommandError(sshErr)
 	if err != nil {
 		return "", err
@@ -68,7 +75,7 @@ func CheckPrivateSshConnection(publicHost Host, privateHost Host, command string
 	}
 
 	// Now connect directly to the privateHost
-	output, sshErr := shell.RunCommandAndGetOutput(shell.Command{Command: "ssh", Args: []string{"-i", publicHost.SshKeyPair.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", publicHost.SshUserName + "@" + publicHost.Hostname, "ssh -i key.pem -o StrictHostKeyChecking=no", privateHost.SshUserName + "@" + privateHost.Hostname, command}}, logger)
+	output, sshErr := shell.RunCommandAndGetOutput(shell.Command{Command: "ssh", Args: []string{"-i", publicKeyPairWithUniqueName.Name, "-o", "UserKnownHostsFile=/dev/null", "-o", "StrictHostKeyChecking=no", publicHost.SshUserName + "@" + publicHost.Hostname, "ssh -i key.pem -o StrictHostKeyChecking=no", privateHost.SshUserName + "@" + privateHost.Hostname, command}}, logger)
 	exitCode, err = shell.GetExitCodeForRunCommandError(sshErr)
 	if err != nil {
 		return output, err
@@ -80,12 +87,22 @@ func CheckPrivateSshConnection(publicHost Host, privateHost Host, command string
 	return output, nil
 }
 
-func writeKeyPairFile(keyPair *terratest.Ec2Keypair, logger *log.Logger) error {
+func writeKeyPairFile(keyPair terratest.Ec2Keypair, logger *log.Logger) error {
 	logger.Println("Creating test-time Key Pair file", keyPair.Name)
 	return ioutil.WriteFile(keyPair.Name, []byte(keyPair.PrivateKey), 0400)
 }
 
-func cleanupKeyPairFile(keyPair *terratest.Ec2Keypair, logger *log.Logger) error {
+func cleanupKeyPairFile(keyPair terratest.Ec2Keypair, logger *log.Logger) error {
 	logger.Println("Cleaning up test-time Key Pair file", keyPair.Name)
 	return os.Remove(keyPair.Name)
+}
+
+// Testing SSH connectivity involves writing and deleting Key Pair files on disk. Since there might be multiple SSH
+// checks happening in parallel, we use this function to give the Key Pair file a unique name, and thereby avoid the
+// files overwriting each other.
+func createKeyPairCopyWithUniqueName(keyPair terratest.Ec2Keypair) terratest.Ec2Keypair {
+	// This automatically creates a shallow copy in Go
+	keyPairWithUniqueName := keyPair
+	keyPairWithUniqueName.Name = fmt.Sprintf("%s-%s", keyPairWithUniqueName.Name, util.UniqueId())
+	return keyPairWithUniqueName
 }
