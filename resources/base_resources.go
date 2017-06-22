59e8a40ea9a3509344adeaeff8adf43589cc9e7f
Matt Calhoun <matt@mattcalhoun.com>
2017-06-22 17:30:05 -0400

    add resource helpers

--- resources/base_resources.go
+++ resources/base_resources.go
@@ -1,1 +1,24 @@
 package resources
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest"
+)
+
+func CreateBaseRandomResourceCollection(t *testing.T, requireNatGateway bool) *terratest.RandomResourceCollection {
+	exludedRegions := []string{REGIONS_WITHOUT_T2_NANO}
+
+	if (requireNatGateway) {
+		exludedRegions = append(exludedRegions, REGIONS_WITHOUT_NAT_GATEWAY_SUPPORT...)
+	}
+
+	resourceCollectionOptions := terratest.NewRandomResourceCollectionOptions()
+	resourceCollectionOptions.ForbiddenRegions = exludedRegions
+
+	randomResourceCollection, err := terratest.CreateRandomResourceCollection(resourceCollectionOptions)
+	if err != nil {
+		t.Fatalf("Failed to create random resource collection: %s\n", err.Error())
+	}
+
+	return randomResourceCollection
+}
