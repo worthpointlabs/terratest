2be806814db25a761b20464b71185902326a4ae6
Matt Calhoun <matt@mattcalhoun.com>
2017-06-23 12:27:19 -0400

    change region exclusion logic

--- resources/base_resources.go
+++ resources/base_resources.go
@@ -5,19 +5,12 @@ import (
 	"github.com/gruntwork-io/terratest"
 )
 
-func CreateBaseRandomResourceCollection(t *testing.T, requireNatGateway bool, requireEcs bool) *terratest.RandomResourceCollection {
-	exludedRegions := REGIONS_WITHOUT_T2_NANO
-
-	if (requireNatGateway) {
-		exludedRegions = append(exludedRegions, REGIONS_WITHOUT_NAT_GATEWAY_SUPPORT...)
-	}
-
-	if (requireEcs) {
-		exludedRegions = append(exludedRegions, REGIONS_WITHOUT_ECS_SUPPORT...)
-	}
-
+func CreateBaseRandomResourceCollection(t *testing.T, excludedRegions ...string) *terratest.RandomResourceCollection {
 	resourceCollectionOptions := terratest.NewRandomResourceCollectionOptions()
-	resourceCollectionOptions.ForbiddenRegions = exludedRegions
+
+	if (excludedRegions!=nil) {
+		resourceCollectionOptions.ForbiddenRegions = excludedRegions
+	}
 
 	randomResourceCollection, err := terratest.CreateRandomResourceCollection(resourceCollectionOptions)
 	if err != nil {
