01cf25438c0995c7015a69b3f0a62f65704b6f4c
Matt Calhoun <matt@mattcalhoun.com>
2017-06-23 12:42:10 -0400

    fix typo

--- resources/exclusions.go
+++ resources/exclusions.go
@@ -1,18 +1,18 @@
 package resources
 
 // As of 4/6/16, these AWS regions do not support the NAT Gateway service.
-const REGIONS_WITHOUT_NAT_GATEWAY_SUPPORT = []string{
+var REGIONS_WITHOUT_NAT_GATEWAY_SUPPORT = []string{
 	"eu-central-1",
 	"ap-northeast-2",
 	"sa-east-1",
 }
 
 // As of 6/9/16, these AWS regions do not support t2.nano instances
-const REGIONS_WITHOUT_T2_NANO = []string{
+var REGIONS_WITHOUT_T2_NANO = []string{
 	"ap-southeast-2",
 }
 
-const REGIONS_WITHOUT_ECS_SUPPORT = []string{
+var REGIONS_WITHOUT_ECS_SUPPORT = []string{
 	"ap-northeast-2",
 	"sa-east-1",
 }
