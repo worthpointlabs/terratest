d7c25ade62f6a60af48ab70190e20009c6d66e72
Matt Calhoun <matt@mattcalhoun.com>
2017-06-29 22:19:28 -0400

    add GetDedicatedTestKeyArn

--- aws/kms.go
+++ aws/kms.go
@@ -3,6 +3,11 @@ package aws
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/gruntwork-io/gruntwork-cli/logging"
+	"github.com/google/uuid"
+	"fmt"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func CreateKmsClient(awsRegion string) (*kms.KMS, error) {
@@ -12,4 +17,21 @@ func CreateKmsClient(awsRegion string) (*kms.KMS, error) {
 	}
 
 	return kms.New(session.New(), awsConfig), nil
+}
+
+func GetDedicatedTestKeyArn(awsRegion string) (string, error) {
+	kmsClient, err := CreateKmsClient(awsRegion)
+	if err != nil {
+		return "", err
+	}
+
+	result, err := kmsClient.DescribeKey(&kms.DescribeKeyInput{
+		KeyId: "alias/dedicated-test-key",
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.KeyMetadata.Arn, nil
 }
