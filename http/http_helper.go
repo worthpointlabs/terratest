e6e49576d83f068ea653eafae562ab1e7bec373d
Yevgeniy Brikman <brikis98@gmail.com>
2018-02-18 16:25:11 +0000

    Add HttpGetWithRetryWithCustomValidation function

--- http/http_helper.go
+++ http/http_helper.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 )
 
+// Perform an HTTP GET on the given URL and return the HTTP status code, body, and any error.
 func HttpGet(url string, logger *log.Logger) (int, string, error) {
 	logger.Println("Making an HTTP GET call to URL", url)
 
@@ -33,6 +34,8 @@ func HttpGet(url string, logger *log.Logger) (int, string, error) {
 	return resp.StatusCode, strings.TrimSpace(string(body)), nil
 }
 
+// Repeatedly perform an HTTP GET on the given URL until a 200 OK with the given body is returned or until max retries
+// has been exceeded.
 func HttpGetWithRetry(url string, expectedBody string, retries int, sleepBetweenRetries time.Duration, logger *log.Logger) error {
 	_, err := util.DoWithRetry(fmt.Sprintf("HTTP GET to URL %s", url), retries, sleepBetweenRetries, logger, func() (string, error) {
 		status, body, err := HttpGet(url, logger)
@@ -45,6 +48,25 @@ func HttpGetWithRetry(url string, expectedBody string, retries int, sleepBetween
 			return "", fmt.Errorf("Got a 200 response, but did not get expected body. Expected: %s. Got: %s.", expectedBody, body)
 		} else {
 			logger.Printf("Got 200 a response from URL %s and expected body: %s\n", url, body)
+			return body, nil
+		}
+	})
+
+	return err
+}
+
+// Repeatedly perform an HTTP GET on the given URL until the given validation function returns true or max retries
+// has been exceeded.
+func HttpGetWithRetryWithCustomValidation(url string, retries int, sleepBetweenRetries time.Duration, logger *log.Logger, validateResponse func(int, string) bool) error {
+	_, err := util.DoWithRetry(fmt.Sprintf("HTTP GET to URL %s", url), retries, sleepBetweenRetries, logger, func() (string, error) {
+		status, body, err := HttpGet(url, logger)
+
+		if err != nil {
+			return "", err
+		} else if !validateResponse(status, body) {
+			return "", fmt.Errorf("Validation function did not pass for status code %d and body %s", status, body)
+		} else {
+			logger.Printf("Validation function passed for URL %s, with status code %d and body: %s\n", url, status, body)
 			return body, nil
 		}
 	})
