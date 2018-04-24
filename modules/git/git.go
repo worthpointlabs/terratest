9330194e4271caa3b44ecde74f48bf17730eee47
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-24 12:52:48 +0100

    Move git package

--- modules/git/git.go
+++ modules/git/git.go
@@ -7,10 +7,18 @@ import (
 )
 
 func GetCurrentBranchName(t *testing.T) string {
+	out, err := GetCurrentBranchNameE(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+func GetCurrentBranchNameE(t *testing.T) (string, error) {
 	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 	bytes, err := cmd.Output()
 	if err != nil {
-		t.Fatalf("Failed to determine current branch name due to error: %s\n", err)
+		return "", err
 	}
-	return strings.TrimSpace(string(bytes))
+	return strings.TrimSpace(string(bytes)), nil
 }
