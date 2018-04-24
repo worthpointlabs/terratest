fe0962f6406c0be2b664e4ba010d375e85bff458
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-24 19:24:54 +0100

    move acm and ami

--- modules/aws/ami_test.go
+++ modules/aws/ami_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestGetUbuntuAmiReturnsSomeAmi(t *testing.T) {
 	t.Parallel()
 
-	amiId := GetUbuntu1404Ami("us-east-1")
+	amiId := GetUbuntu1404Ami(t, "us-east-1")
 	if amiId[:4] != "ami-" {
 		t.Fatalf("Expected: string formatted like ami-*******. Received: %s", amiId)
 	}
