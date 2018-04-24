d10e83c61711a16059d79f32fa5e36424e875eb8
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-24 12:24:39 +0100

    Update random code

--- util/random.go
+++ util/random.go
@@ -18,6 +18,12 @@ func RandomInt(elements []int) int {
 	return elements[index]
 }
 
+// Pick a random element in the slice of string
+func RandomString(elements []string) string {
+	index := Random(0, len(elements))
+	return elements[index]
+}
+
 // Returns a unique (ish) id we can attach to resources and tfstate files so they don't conflict with each other
 // Uses base 62 to generate a 6 character string that's unlikely to collide with the handful of tests we run in
 // parallel. Based on code here: http://stackoverflow.com/a/9543797/483528
@@ -28,9 +34,9 @@ func UniqueId() string {
 
 	var out bytes.Buffer
 
-	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < UNIQUE_ID_LENGTH; i++ {
-		out.WriteByte(BASE_62_CHARS[rand.Intn(len(BASE_62_CHARS))])
+		out.WriteByte(BASE_62_CHARS[generator.Intn(len(BASE_62_CHARS))])
 	}
 
 	return out.String()
