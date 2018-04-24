9ba1eb079d1266cb11a8b601e53350f5e4ddd1be
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-24 19:52:46 +0100

    Move ec2

--- modules/aws/ec2.go
+++ modules/aws/ec2.go
@@ -2,7 +2,94 @@ package aws
 
 import (
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/aws"
+	"fmt"
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/logger"
 )
+
+// Return all the IDs of EC2 instances in the given region with the given tag
+func GetEc2InstanceIdsByTag(t *testing.T, region string, tagName string, tagValue string) []string {
+	out, err := GetEc2InstanceIdsByTagE(t, region, tagName, tagValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+// Return all the IDs of EC2 instances in the given region with the given tag
+func GetEc2InstanceIdsByTagE(t *testing.T, region string, tagName string, tagValue string) ([]string, error) {
+	client, err := NewEc2Client(region)
+	if err != nil {
+		return nil, err
+	}
+
+	tagFilter := &ec2.Filter{
+		Name: aws.String(fmt.Sprintf("tag:%s", tagName)),
+		Values: []*string{aws.String(tagValue)},
+	}
+	output, err := client.DescribeInstances(&ec2.DescribeInstancesInput{Filters: []*ec2.Filter{tagFilter}})
+	if err != nil {
+		return nil, err
+	}
+
+	instanceIds := []string{}
+
+	for _, reservation := range output.Reservations {
+		for _, instance := range reservation.Instances {
+			instanceIds = append(instanceIds, *instance.InstanceId)
+		}
+	}
+
+	return instanceIds, err
+}
+
+// Delete the given AMI in the given region
+func DeleteAmi(t *testing.T, region string, imageId string) {
+	err := DeleteAmiE(t, region, imageId)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Delete the given AMI in the given region
+func DeleteAmiE(t *testing.T, region string, imageId string) error {
+	logger.Logf(t, "Deregistering AMI %s", imageId)
+
+	client, err := NewEc2Client(region)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.DeregisterImage(&ec2.DeregisterImageInput{ImageId: aws.String(imageId)})
+	return err
+}
+
+// Terminate the EC2 instance with the given ID in the given region
+func TerminateInstance(t *testing.T, region string, instanceId string) {
+	err := TerminateInstanceE(t, region, instanceId)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Terminate the EC2 instance with the given ID in the given region
+func TerminateInstanceE(t *testing.T, region string, instanceId string) error {
+	logger.Logf(t, "Terminating Instance %s", instanceId)
+
+	client, err := NewEc2Client(region)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.TerminateInstances(&ec2.TerminateInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instanceId),
+		},
+	})
+
+	return err
+}
 
 // Create an EC2 client
 func NewEc2Client(region string) (*ec2.EC2, error) {
