a768b225c257ae697b956106ef45f79dc5894978
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-26 01:35:05 +0100

    Remove UniqueId from Options

--- modules/terraform/options.go
+++ modules/terraform/options.go
@@ -4,7 +4,6 @@ import "time"
 
 // Options for running Terraform commands
 type Options struct {
-	UniqueId                 string                 // A unique identifier that can be used to namespace resources.
 	TerraformDir             string                 // The path to the folder where the Terraform code is defined.
 	Vars                     map[string]interface{} // The vars to pass to Terraform commands using the -var option.
 	RetryableTerraformErrors map[string]string      // If Terraform apply fails with one of these (transient) errors, retry. The keys are text to look for in the error and the message is what to display to a user if that error is found.
