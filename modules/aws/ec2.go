1a16811a6d9dabbf14f2a7a1e43fbb212ba8cc0a
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-29 22:16:43 +0100

    Add GetPublicIpOfEc2Instance helper

--- modules/aws/ec2.go
+++ modules/aws/ec2.go
@@ -8,6 +8,34 @@ import (
 	"github.com/gruntwork-io/terratest/modules/logger"
 )
 
+// Get the public IP address of the given EC2 Instance in the given region
+func GetPublicIpOfEc2Instance(t *testing.T, instanceId string, awsRegion string) string {
+	ip, err := GetPublicIpOfEc2InstanceE(t, instanceId, awsRegion)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return ip
+}
+
+// Get the public IP address of the given EC2 Instance in the given region
+func GetPublicIpOfEc2InstanceE(t *testing.T, instanceId string, awsRegion string) (string, error) {
+	ec2Client := NewEc2Client(t, awsRegion)
+
+	input := ec2.DescribeInstancesInput{InstanceIds: []*string{aws.String(instanceId)}}
+	output, err := ec2Client.DescribeInstances(&input)
+	if err != nil {
+		return "", err
+	}
+
+	for _, reserveration := range output.Reservations {
+		for _, instance := range reserveration.Instances {
+			return aws.StringValue(instance.PublicIpAddress), nil
+		}
+	}
+
+	return "", IpForEc2InstanceNotFound{InstanceId: instanceId, AwsRegion: awsRegion}
+}
+
 // Return all the IDs of EC2 instances in the given region with the given tag
 func GetEc2InstanceIdsByTag(t *testing.T, region string, tagName string, tagValue string) []string {
 	out, err := GetEc2InstanceIdsByTagE(t, region, tagName, tagValue)
@@ -25,7 +53,7 @@ func GetEc2InstanceIdsByTagE(t *testing.T, region string, tagName string, tagVal
 	}
 
 	tagFilter := &ec2.Filter{
-		Name: aws.String(fmt.Sprintf("tag:%s", tagName)),
+		Name:   aws.String(fmt.Sprintf("tag:%s", tagName)),
 		Values: []*string{aws.String(tagValue)},
 	}
 	output, err := client.DescribeInstances(&ec2.DescribeInstancesInput{Filters: []*ec2.Filter{tagFilter}})
@@ -63,11 +91,11 @@ func GetTagsForEc2InstanceE(t *testing.T, region string, instanceId string) (map
 	input := ec2.DescribeTagsInput{
 		Filters: []*ec2.Filter{
 			{
-				Name: aws.String("resource-type"),
+				Name:   aws.String("resource-type"),
 				Values: aws.StringSlice([]string{"instance"}),
 			},
 			{
-				Name: aws.String("resource-id"),
+				Name:   aws.String("resource-id"),
 				Values: aws.StringSlice([]string{instanceId}),
 			},
 		},
@@ -151,4 +179,13 @@ func NewEc2ClientE(t *testing.T, region string) (*ec2.EC2, error) {
 	}
 
 	return ec2.New(sess), nil
+}
+
+type IpForEc2InstanceNotFound struct {
+	InstanceId string
+	AwsRegion  string
+}
+
+func (err IpForEc2InstanceNotFound) Error() string {
+	return fmt.Sprintf("Could not find a public IP address for EC2 Instance %s in %s", err.InstanceId, err.AwsRegion)
 }
