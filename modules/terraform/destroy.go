481f3ba22aebf144c76c24bf72db2bd53b95d921
Yevgeniy Brikman <brikis98@gmail.com>
2018-05-05 15:42:56 +0100

    Fix destroy command

--- modules/terraform/destroy.go
+++ modules/terraform/destroy.go
@@ -15,5 +15,5 @@ func Destroy(t *testing.T, options *Options) string {
 
 // Run terraform destroy with the given options and return stdout/stderr.
 func DestroyE(t *testing.T, options *Options) (string, error) {
-	return RunTerraformCommandE(t, options, "destroy", "-force", "-input=false", "-lock=false")
+	return RunTerraformCommandE(t, options, FormatArgs(options.Vars, "destroy", "-force", "-input=false", "-lock=false")...)
 }
