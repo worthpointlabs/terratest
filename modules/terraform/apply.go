c6634fa7cbaacf28397aa75ffed996c65794f9c3
Yevgeniy Brikman <brikis98@gmail.com>
2018-05-05 15:41:16 +0100

    Fix apply command args

--- modules/terraform/apply.go
+++ modules/terraform/apply.go
@@ -43,5 +43,5 @@ func Apply(t *testing.T, options *Options) string {
 // Run terraform apply with the given options and return stdout/stderr. Note that this method does NOT call destroy and
 // assumes the caller is responsible for cleaning up any resources created by running apply.
 func ApplyE(t *testing.T, options *Options) (string, error) {
-	return RunTerraformCommandE(t, options, "apply", "-input=false", "-lock=false", "-auto-approve")
+	return RunTerraformCommandE(t, options, FormatArgs(options.Vars, "apply", "-input=false", "-lock=false", "-auto-approve")...)
 }
