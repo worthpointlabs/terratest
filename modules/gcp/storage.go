c5f85c2dc707e917362356a3b5892e98297bc337
Rob Morgan <robbym@gmail.com>
2018-08-09 11:50:04 +0200

    rework storage functions a bit. add new test methods

--- modules/gcp/storage.go
+++ modules/gcp/storage.go
@@ -2,7 +2,6 @@ package gcp
 
 import (
 	"context"
-	"log"
 	"testing"
 
 	"cloud.google.com/go/storage"
@@ -10,15 +9,15 @@ import (
 )
 
 // CreateStorageBucket creates a Google Cloud bucket with the given BucketAttrs. Note that Google Storage bucket names must be globally unique.
-func CreateStorageBucket(t *testing.T, projectId string, name string, attr *storage.BucketAttrs) {
-	err := CreateStorageBucketE(t, projectId, name, attr)
+func CreateStorageBucket(t *testing.T, projectID string, name string, attr *storage.BucketAttrs) {
+	err := CreateStorageBucketE(t, projectID, name, attr)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 // CreateStorageBucketE creates a Google Cloud bucket with the given BucketAttrs. Note that Google Storage bucket names must be globally unique.
-func CreateStorageBucketE(t *testing.T, projectId string, name string, attr *storage.BucketAttrs) error {
+func CreateStorageBucketE(t *testing.T, projectID string, name string, attr *storage.BucketAttrs) error {
 	logger.Logf(t, "Creating bucket %s", name)
 
 	ctx := context.Background()
@@ -26,21 +25,14 @@ func CreateStorageBucketE(t *testing.T, projectId string, name string, attr *sto
 	// Creates a client.
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
+		return err
 	}
-
-	// Sets the name for the new bucket.
-	bucketName := name
 
 	// Creates a Bucket instance.
-	bucket := client.Bucket(bucketName)
+	bucket := client.Bucket(name)
 
 	// Creates the new bucket.
-	if err := bucket.Create(ctx, projectId, attr); err != nil {
-		log.Fatalf("Failed to create bucket: %v", err)
-	}
-
-	return err
+	return bucket.Create(ctx, projectID, attr)
 }
 
 // DeleteStorageBucket destroys the Google Storage bucket.
@@ -52,7 +44,7 @@ func DeleteStorageBucket(t *testing.T, name string) {
 	}
 }
 
-// DeleteS3BucketE destroys the S3 bucket in the given region with the given name.
+// DeleteStorageBucketE destroys the S3 bucket in the given region with the given name.
 func DeleteStorageBucketE(t *testing.T, name string) error {
 	logger.Logf(t, "Deleting bucket %s", name)
 
@@ -60,12 +52,44 @@ func DeleteStorageBucketE(t *testing.T, name string) error {
 
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	if err := client.Bucket(name).Delete(ctx); err != nil {
 		return err
 	}
 
-	return err
+	return client.Bucket(name).Delete(ctx)
+}
+
+// AssertStorageBucketExists checks if the given storage bucket exists and fails the test if it does not.
+func AssertStorageBucketExists(t *testing.T, name string) {
+	err := AssertStorageBucketExistsE(t, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// AssertStorageBucketExistsE checks if the given storage bucket exists and returns an error if it does not.
+func AssertStorageBucketExistsE(t *testing.T, name string) error {
+	logger.Logf(t, "Finding bucket %s", name)
+
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Creates a Bucket instance.
+	bucket := client.Bucket(name)
+
+	if _, err := bucket.Attrs(ctx); err != nil {
+		// ErrBucketNotExist
+		return err
+	}
+
+	it := bucket.Objects(ctx, nil)
+	if _, err := it.Next(); err == storage.ErrBucketNotExist {
+		return err
+	}
+
+	return nil
 }
--- modules/gcp/storage_test.go
+++ modules/gcp/storage_test.go
@@ -24,3 +24,31 @@ func TestCreateAndDestroyStorageBucket(t *testing.T) {
 	CreateStorageBucket(t, projectId, gsBucketName, nil)
 	DeleteStorageBucket(t, gsBucketName)
 }
+
+func TestAssertStorageBucketExistsNoFalseNegative(t *testing.T) {
+	t.Parallel()
+
+	id := random.UniqueId()
+	gsBucketName := "gruntwork-terratest-" + strings.ToLower(id)
+	logger.Logf(t, "Random values selected Id = %s\n", id)
+
+	CreateStorageBucket(t, projectId, gsBucketName, nil)
+	defer DeleteStorageBucket(t, gsBucketName)
+
+	AssertStorageBucketExists(t, gsBucketName)
+}
+
+func TestAssertStorageBucketExistsNoFalsePositive(t *testing.T) {
+	t.Parallel()
+
+	id := random.UniqueId()
+	gsBucketName := "gruntwork-terratest-" + strings.ToLower(id)
+	logger.Logf(t, "Random values selected Id = %s\n", id)
+
+	// Don't create a new storage bucket so we can confirm that our function works as expected.
+
+	err := AssertStorageBucketExistsE(t, gsBucketName)
+	if err == nil {
+		t.Fatalf("Function claimed that the Storage Bucket '%s' exists, but in fact it does not.", gsBucketName)
+	}
+}
