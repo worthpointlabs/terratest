ff939359fba8acf3e3c8b295960c6d4d6725605d
Josh Purvis <joshua.purvis@gmail.com>
2018-08-28 09:38:43 -0400

    Remove incorrect defer and change test to check for the parent socketDir instead

--- modules/ssh/agent_test.go
+++ modules/ssh/agent_test.go
@@ -11,10 +11,10 @@ import (
 func TestSshAgentWithKeyPair(t *testing.T) {
 	keyPair := GenerateRSAKeyPair(t, 2048)
 	sshAgent := SshAgentWithKeyPair(t, keyPair)
-	defer sshAgent.Stop()
 
 	// ensure that socket directory is set in environment, and it exists
 	sockFile := filepath.Join(sshAgent.socketDir, "ssh_auth.sock")
+
 	sockFileEnv, found := os.LookupEnv("SSH_AUTH_SOCK")
 	assert.FileExists(t, sockFile)
 	assert.True(t, found)
@@ -24,9 +24,10 @@ func TestSshAgentWithKeyPair(t *testing.T) {
 	keys, _ := sshAgent.agent.List()
 	assert.Len(t, keys, 1)
 
+	sshAgent.Stop()
 
 	// is socketDir removed as expected?
-	if _, err := os.Stat(sockFile); os.IsExist(err) {
+	if _, err := os.Stat(sshAgent.socketDir); !os.IsNotExist(err) {
 		assert.FailNow(t, "ssh agent failed to remove socketDir on Stop()")
 	}
 }
