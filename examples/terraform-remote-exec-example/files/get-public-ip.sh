9f44bd1dee8fa9ffd1915a10a66c7d84c837d941
Josh Purvis <joshua.purvis@gmail.com>
2018-08-31 13:43:30 -0400

    remove redundant echo in example

--- examples/terraform-remote-exec-example/files/get-public-ip.sh
+++ examples/terraform-remote-exec-example/files/get-public-ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # For example purposes, print the public IP address of this instance
-echo $(wget -q -O- http://169.254.169.254/latest/meta-data/public-ipv4)
+wget -q -O- http://169.254.169.254/latest/meta-data/public-ipv4
 
