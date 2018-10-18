a672fff31fc0827e88bf59ac42a4b6d696f9d0ce
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2018-10-18 09:57:26 -0700

    [skip ci] foo

--- aws/account.go
+++ aws/account.go
@@ -1,20 +1,21 @@
 package aws
+
 import (
-	"github.com/aws/aws-sdk-go/service/iam"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"strings"
 	"errors"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"strings"
 )
 
 // Get the Account ID for the currently logged in IAM User.
 func GetAccountId() (string, error) {
-	svc := iam.New(session.New())
-	user, err := svc.GetUser(&iam.GetUserInput{})
+	svc := sts.New(session.New())
+	callerIdentity, err := svc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
 	if err != nil {
 		return "", err
 	}
 
-	return extractAccountIdFromArn(*user.User.Arn)
+	return *callerIdentity.Account, nil
 }
 
 // An IAM arn is of the format arn:aws:iam::123456789012:user/test. The account id is the number after arn:aws:iam::,
