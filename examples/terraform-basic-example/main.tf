d9690bb1509046f7e29282329b39dc633a6101a4
Alex Chaplianka <alexettelis@gmail.com>
2018-12-03 13:02:56 +0200

    Adding test for the -target parameter
    The local_file has both data so the output would have both of them too

--- examples/terraform-basic-example/main.tf
+++ examples/terraform-basic-example/main.tf
@@ -10,3 +10,12 @@ data "template_file" "example" {
 data "template_file" "example2" {
   template = "${var.example2}"
 }
+resource "local_file" "example" {
+  content  = "${data.template_file.example.rendered} + ${data.template_file.example2.rendered}"
+  filename = "example.txt"
+}
+
+resource "local_file" "example2" {
+  content  = "${data.template_file.example2.rendered}"
+  filename = "example2.txt"
+}
--- test/terraform_basic_example_test.go
+++ test/terraform_basic_example_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"path/filepath"
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/files"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -10,17 +13,22 @@ import (
 // An example of how to test the simple Terraform module in examples/terraform-basic-example using Terratest.
 func TestTerraformBasicExample(t *testing.T) {
 	t.Parallel()
+	terraformDir := "../examples/terraform-basic-example"
 
 	expectedText := "foo"
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
-		TerraformDir: "../examples/terraform-basic-example",
+		TerraformDir: terraformDir,
 
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
 			"example": expectedText,
 		},
+		// Only create the example1 file
+		Targets: []string{"local_file.example"},
+
+		// Use the var files
 		VarFiles: []string{"varfile.tfvars"},
 
 		NoColor: true,
@@ -41,5 +49,10 @@ func TestTerraformBasicExample(t *testing.T) {
 	// Test for the second variable which comes from the var file
 	actualText = terraform.Output(t, terraformOptions, "example2")
 	assert.Equal(t, "test", actualText)
+
+	// Test resources: the target specifies only the local_file.example resource
+	//only the file  example.txt should be created, and example2.txt should NOT
+	assert.True(t, files.FileExists(filepath.Join(terraformDir, "example.txt")))
+	assert.False(t, files.FileExists(filepath.Join(terraformDir, "example2.txt")))
 
 }
