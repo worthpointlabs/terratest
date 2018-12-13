910a45b1273d67277b323d33ce09a4c3a162e366
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2018-12-13 07:18:46 -0800

    Add test to expect order

--- modules/shell/command_test.go
+++ modules/shell/command_test.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -18,4 +19,24 @@ func TestRunCommandAndGetOutput(t *testing.T) {
 
 	out := RunCommandAndGetOutput(t, cmd)
 	assert.Equal(t, text, strings.TrimSpace(out))
+}
+
+func TestRunCommandAndGetOutputOrder(t *testing.T) {
+	t.Parallel()
+
+	stdoutText := "Hello, Error"
+	stderrText := "Hello, World"
+	expectedText := "Hello, Error\nHello, World"
+	pythonCode := fmt.Sprintf(
+		"from __future__ import print_function; import sys; print('%s', file=sys.stderr); print('%s', file=sys.stdout)",
+		stderrText,
+		stdoutText,
+	)
+	cmd := Command{
+		Command: "python",
+		Args:    []string{"-c", pythonCode},
+	}
+
+	out := RunCommandAndGetOutput(t, cmd)
+	assert.Equal(t, strings.TrimSpace(out), expectedText)
 }
