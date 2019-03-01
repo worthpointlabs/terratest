1ef184e9de36d17c2394584e5d5e4071a6f5f0eb
kirecek <erikjankovic@gmail.com>
2019-03-01 14:53:53 +0100

    Add Get helper for cluster role
    Signed-off-by: kirecek <erikjankovic@gmail.com>

---
+++ modules/k8s/cluster_role.go
@@ -0,0 +1,23 @@
+package k8s
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GetClusterRole returns a Kubernetes role resource with the given name. This will fail the test if there is an error.
+func GetClusterRole(t *testing.T, options *KubectlOptions, roleName string) *rbacv1.ClusterRole {
+	role, err := GetClusterRoleE(t, options, roleName)
+	require.NoError(t, err)
+	return role
+}
+
+// GetRole returns a Kubernetes role resource with the given name.
+func GetClusterRoleE(t *testing.T, options *KubectlOptions, roleName string) (*rbacv1.ClusterRole, error) {
+	clientset, err := GetKubernetesClientFromOptionsE(t, options)
+	require.NoError(t, err)
+	return clientset.RbacV1().ClusterRoles().Get(roleName, metav1.GetOptions{})
+}
