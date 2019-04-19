84b5b14d4fc87251693939a56fd602f591850e2d
Stas Alekseev <stas.alekseev@gmail.com>
2019-04-19 15:44:30 -0400

    Add module/aws/ssm

---
+++ modules/aws/ssm.go
@@ -0,0 +1,72 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func GetParameter(t *testing.T, awsRegion string, keyName string) string {
+	keyValue, err := GetParameterE(t, awsRegion, keyName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return keyValue
+}
+
+func GetParameterE(t *testing.T, awsRegion string, keyName string) (string, error) {
+	ssmClient, err := NewSsmClientE(t, awsRegion)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := ssmClient.GetParameter(&ssm.GetParameterInput{Name: aws.String(keyName), WithDecryption: aws.Bool(true)})
+	if err != nil {
+		return "", err
+	}
+
+	parameter := *resp.Parameter
+	return *parameter.Value, nil
+}
+
+func PutParameter(t *testing.T, awsRegion string, keyName string, keyDescription string, keyValue string) int64 {
+	version, err := PutParameterE(t, awsRegion, keyName, keyDescription, keyValue)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return version
+}
+
+func PutParameterE(t *testing.T, awsRegion string, keyName string, keyDescription string, keyValue string) (int64, error) {
+	ssmClient, err := NewSsmClientE(t, awsRegion)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := ssmClient.PutParameter(&ssm.PutParameterInput{Name: aws.String(keyName), Description: aws.String(keyDescription), Value: aws.String(keyValue), Type: aws.String("SecureString")})
+	if err != nil {
+		return 0, err
+	}
+
+	return *resp.Version, nil
+}
+
+// NewSsmClient creates a ssm client.
+func NewSsmClient(t *testing.T, region string) *ssm.SSM {
+	client, err := NewSsmClientE(t, region)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return client
+}
+
+// NewSsmClientE creates an ssm client.
+func NewSsmClientE(t *testing.T, region string) (*ssm.SSM, error) {
+	sess, err := NewAuthenticatedSession(region)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssm.New(sess), nil
+}
