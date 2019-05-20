b0b61c2a6fae97d760707caed827b69e9867be73
Yevgeniy Brikman <brikis98@gmail.com>
2019-05-20 12:28:26 +0100

    Fix backend config params
    Terraform seems to require that backend config arguments are passed using the syntax `-backend-config=key=value`. That is, with an equals sign as a separator. The syntax we use elsewhere with a space as a separator (e.g., `-var key=value`) doesn't seem to work for backend config. Added example + test to verify too.

---
+++ examples/terraform-backend-example/main.tf
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# AN EXAMPLE OF HOW TO CONFIGURE A TERRAFORM BACKEND WITH TERRATEST
+# Note that the example code here doesn't do anything other than set up a backend that Terratest will configure.
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # Leave the config for this backend unspecified so Terraform can fill it in. This is known as "partial configuration":
+  # https://www.terraform.io/docs/backends/config.html#partial-configuration
+  backend "s3" {}
+}
+
+output "foo" {
+  value = "bar"
+}
---
+++ test/terraform_backend_example_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// An example of how to test the Terraform module in examples/terraform-backend-example using Terratest.
+func TestTerraformBackendExample(t *testing.T) {
+	t.Parallel()
+
+	awsRegion := aws.GetRandomRegion(t, nil, nil)
+	uniqueId := random.UniqueId()
+
+	// Create an S3 bucket where we can store state
+	bucketName := fmt.Sprintf("test-terraform-backend-example-%s", uniqueId)
+	defer aws.DeleteS3Bucket(t, awsRegion, bucketName)
+	aws.CreateS3Bucket(t, awsRegion, bucketName)
+
+	// Deploy the module, configuring it to use the S3 bucket as an S3 backend
+	terraformOptions := &terraform.Options{
+		BackendConfig: map[string]interface{}{
+			"bucket": bucketName,
+			"key":    "terraform.tfstate",
+			"region": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	// The module doesn't really *do* anything, so we just check a dummy output here and move on
+	foo := terraform.OutputRequired(t, terraformOptions, "foo")
+	require.Equal(t, "bar", foo)
+}
