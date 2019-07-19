9581c57f716db4ae24e2436568b5b4c761ed9a21
Mitch Robins <mitch.robins@optum.com>
2019-07-19 07:24:11 -0500

    Changing variable naming to follow better Go convention Changing HTTP config to only pass transport options to the local client

--- modules/http-helper/http_helper.go
+++ modules/http-helper/http_helper.go
@@ -16,8 +16,8 @@ import (
 
 // HttpGet performs an HTTP GET, with an optional pointer to a custom TLS configuration, on the given URL and
 // return the HTTP status code and body. If there's any error, fail the test.
-func HttpGet(t *testing.T, url string, TLSConfig *tls.Config) (int, string) {
-	statusCode, body, err := HttpGetE(t, url, TLSConfig)
+func HttpGet(t *testing.T, url string, tlsConfig *tls.Config) (int, string) {
+	statusCode, body, err := HttpGetE(t, url, tlsConfig)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,14 +26,19 @@ func HttpGet(t *testing.T, url string, TLSConfig *tls.Config) (int, string) {
 
 // HttpGetE performs an HTTP GET, with an optional pointer to a custom TLS configuration, on the given URL and
 // return the HTTP status code, body, and any error.
-func HttpGetE(t *testing.T, url string, TLSConfig *tls.Config) (int, string, error) {
+func HttpGetE(t *testing.T, url string, tlsConfig *tls.Config) (int, string, error) {
 	logger.Logf(t, "Making an HTTP GET call to URL %s", url)
 
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = TLSConfig
+	// Set HTTP client transport config
+	tr := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
 
 	client := http.Client{
 		// By default, Go does not impose a timeout, so an HTTP connection attempt can hang for a LONG time.
 		Timeout: 10 * time.Second,
+		// Include the previously created transport config
+		Transport: tr,
 	}
 
 	resp, err := client.Get(url)
@@ -53,8 +58,8 @@ func HttpGetE(t *testing.T, url string, TLSConfig *tls.Config) (int, string, err
 
 // HttpGetWithValidation performs an HTTP GET on the given URL and verify that you get back the expected status code and body. If either
 // doesn't match, fail the test.
-func HttpGetWithValidation(t *testing.T, url string, TLSConfig *tls.Config, expectedStatusCode int, expectedBody string) {
-	err := HttpGetWithValidationE(t, url, TLSConfig, expectedStatusCode, expectedBody)
+func HttpGetWithValidation(t *testing.T, url string, tlsConfig *tls.Config, expectedStatusCode int, expectedBody string) {
+	err := HttpGetWithValidationE(t, url, tlsConfig, expectedStatusCode, expectedBody)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -62,23 +67,23 @@ func HttpGetWithValidation(t *testing.T, url string, TLSConfig *tls.Config, expe
 
 // HttpGetWithValidationE performs an HTTP GET on the given URL and verify that you get back the expected status code and body. If either
 // doesn't match, return an error.
-func HttpGetWithValidationE(t *testing.T, url string, TLSConfig *tls.Config, expectedStatusCode int, expectedBody string) error {
-	return HttpGetWithCustomValidationE(t, url, TLSConfig, func(statusCode int, body string) bool {
+func HttpGetWithValidationE(t *testing.T, url string, tlsConfig *tls.Config, expectedStatusCode int, expectedBody string) error {
+	return HttpGetWithCustomValidationE(t, url, tlsConfig, func(statusCode int, body string) bool {
 		return statusCode == expectedStatusCode && body == expectedBody
 	})
 }
 
 // HttpGetWithCustomValidation performs an HTTP GET on the given URL and validate the returned status code and body using the given function.
-func HttpGetWithCustomValidation(t *testing.T, url string, TLSConfig *tls.Config, validateResponse func(int, string) bool) {
-	err := HttpGetWithCustomValidationE(t, url, TLSConfig, validateResponse)
+func HttpGetWithCustomValidation(t *testing.T, url string, tlsConfig *tls.Config, validateResponse func(int, string) bool) {
+	err := HttpGetWithCustomValidationE(t, url, tlsConfig, validateResponse)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 // HttpGetWithCustomValidationE performs an HTTP GET on the given URL and validate the returned status code and body using the given function.
-func HttpGetWithCustomValidationE(t *testing.T, url string, TLSConfig *tls.Config, validateResponse func(int, string) bool) error {
-	statusCode, body, err := HttpGetE(t, url, TLSConfig)
+func HttpGetWithCustomValidationE(t *testing.T, url string, tlsConfig *tls.Config, validateResponse func(int, string) bool) error {
+	statusCode, body, err := HttpGetE(t, url, tlsConfig)
 
 	if err != nil {
 		return err
@@ -93,8 +98,8 @@ func HttpGetWithCustomValidationE(t *testing.T, url string, TLSConfig *tls.Confi
 
 // HttpGetWithRetry repeatedly performs an HTTP GET on the given URL until the given status code and body are returned or until max
 // retries has been exceeded.
-func HttpGetWithRetry(t *testing.T, url string, TLSConfig *tls.Config, expectedStatus int, expectedBody string, retries int, sleepBetweenRetries time.Duration) {
-	err := HttpGetWithRetryE(t, url, TLSConfig, expectedStatus, expectedBody, retries, sleepBetweenRetries)
+func HttpGetWithRetry(t *testing.T, url string, tlsConfig *tls.Config, expectedStatus int, expectedBody string, retries int, sleepBetweenRetries time.Duration) {
+	err := HttpGetWithRetryE(t, url, tlsConfig, expectedStatus, expectedBody, retries, sleepBetweenRetries)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -102,9 +107,9 @@ func HttpGetWithRetry(t *testing.T, url string, TLSConfig *tls.Config, expectedS
 
 // HttpGetWithRetryE repeatedly performs an HTTP GET on the given URL until the given status code and body are returned or until max
 // retries has been exceeded.
-func HttpGetWithRetryE(t *testing.T, url string, TLSConfig *tls.Config, expectedStatus int, expectedBody string, retries int, sleepBetweenRetries time.Duration) error {
+func HttpGetWithRetryE(t *testing.T, url string, tlsConfig *tls.Config, expectedStatus int, expectedBody string, retries int, sleepBetweenRetries time.Duration) error {
 	_, err := retry.DoWithRetryE(t, fmt.Sprintf("HTTP GET to URL %s", url), retries, sleepBetweenRetries, func() (string, error) {
-		return "", HttpGetWithValidationE(t, url, TLSConfig, expectedStatus, expectedBody)
+		return "", HttpGetWithValidationE(t, url, tlsConfig, expectedStatus, expectedBody)
 	})
 
 	return err
@@ -112,8 +117,8 @@ func HttpGetWithRetryE(t *testing.T, url string, TLSConfig *tls.Config, expected
 
 // HttpGetWithRetryWithCustomValidation repeatedly performs an HTTP GET on the given URL until the given validation function returns true or max retries
 // has been exceeded.
-func HttpGetWithRetryWithCustomValidation(t *testing.T, url string, TLSConfig *tls.Config, retries int, sleepBetweenRetries time.Duration, validateResponse func(int, string) bool) {
-	err := HttpGetWithRetryWithCustomValidationE(t, url, TLSConfig, retries, sleepBetweenRetries, validateResponse)
+func HttpGetWithRetryWithCustomValidation(t *testing.T, url string, tlsConfig *tls.Config, retries int, sleepBetweenRetries time.Duration, validateResponse func(int, string) bool) {
+	err := HttpGetWithRetryWithCustomValidationE(t, url, tlsConfig, retries, sleepBetweenRetries, validateResponse)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -121,9 +126,9 @@ func HttpGetWithRetryWithCustomValidation(t *testing.T, url string, TLSConfig *t
 
 // HttpGetWithRetryWithCustomValidationE repeatedly performs an HTTP GET on the given URL until the given validation function returns true or max retries
 // has been exceeded.
-func HttpGetWithRetryWithCustomValidationE(t *testing.T, url string, TLSConfig *tls.Config, retries int, sleepBetweenRetries time.Duration, validateResponse func(int, string) bool) error {
+func HttpGetWithRetryWithCustomValidationE(t *testing.T, url string, tlsConfig *tls.Config, retries int, sleepBetweenRetries time.Duration, validateResponse func(int, string) bool) error {
 	_, err := retry.DoWithRetryE(t, fmt.Sprintf("HTTP GET to URL %s", url), retries, sleepBetweenRetries, func() (string, error) {
-		return "", HttpGetWithCustomValidationE(t, url, TLSConfig, validateResponse)
+		return "", HttpGetWithCustomValidationE(t, url, tlsConfig, validateResponse)
 	})
 
 	return err
