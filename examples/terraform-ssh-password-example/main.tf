5235dd46798726e18a3081a25309a53fd7e27de9
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-26 15:53:35 +0100

    Duplicate and adapt example Terraform stack around new test.

---
+++ examples/terraform-ssh-password-example/main.tf
@@ -0,0 +1,54 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY AN EC2 INSTANCE THAT ALLOWS CONNECTIONS VIA SSH
+# See test/terraform_ssh_password_example.go for how to write automated tests for this code.
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE EC2 INSTANCE WITH A PUBLIC IP
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_instance" "example_public" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  user_data     = "${data.template_file.user_data.rendered}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.example.id}",
+  ]
+
+  # This EC2 Instance has a public IP and will be accessible directly from the public Internet
+  associate_public_ip_address = "true"
+
+  tags {
+    Name = "${var.instance_name}-public"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A SECURITY GROUP TO CONTROL WHAT REQUESTS CAN GO IN AND OUT OF THE EC2 INSTANCE
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "example" {
+  name = "${var.instance_name}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = "${var.ssh_port}"
+    to_port   = "${var.ssh_port}"
+    protocol  = "tcp"
+
+    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
+    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
