98a6957e29cb76b21f2ed48355bad0cb31222b48
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-08-02 07:43:21 -0700

    Add apt-get failure to packer retries

--- test/packer_basic_example_test.go
+++ test/packer_basic_example_test.go
@@ -20,7 +20,8 @@ import (
 // to make our tests resilient to that by specifying those known common errors here and telling our builds to retry if
 // they hit those errors.
 var DefaultRetryablePackerErrors = map[string]string{
-	"Script disconnected unexpectedly": "Occasionally, Packer seems to lose connectivity to AWS, perhaps due to a brief network outage",
+	"Script disconnected unexpectedly":                                                 "Occasionally, Packer seems to lose connectivity to AWS, perhaps due to a brief network outage",
+	"can not open /var/lib/apt/lists/archive.ubuntu.com_ubuntu_dists_xenial_InRelease": "Occasionally, apt-get fails on ubuntu to update the cache",
 }
 var DefaultTimeBetweenPackerRetries = 15 * time.Second
 
