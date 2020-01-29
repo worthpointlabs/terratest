526ae06f930b79adfde3579ed8f12c82172a3d83
Yevgeniy Brikman <brikis98@gmail.com>
2020-01-29 16:13:27 +0000

    Clean up Azure example

--- test/terraform_azure_example_test.go
+++ test/terraform_azure_example_test.go
@@ -14,30 +14,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// An example of how to test the Terraform module in examples/terraform-azure-example using Terratest.
 func TestTerraformAzureExample(t *testing.T) {
 	t.Parallel()
 
-	// website::tag::1::Configure Terraform setting up a path to Terraform code.
+	// website::tag::1:: Configure Terraform setting up a path to Terraform code.
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: "../examples/terraform-azure-example",
 	}
 
-	// website::tag::4::At the end of the test, run `terraform destroy` to clean up any resources that were created
+	// website::tag::4:: At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// website::tag::2::This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	// website::tag::2:: Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of an output variable
+	// website::tag::3:: Run `terraform output` to get the values of output variables
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-	// Look up the size of the given Virtual Machine
+	// website::tag::4:: Look up the size of the given Virtual Machine and ensure it matches the output.
 	actualVMSize := azure.GetSizeOfVirtualMachine(t, vmName, resourceGroupName, "")
 	expectedVMSize := compute.VirtualMachineSizeTypes("Standard_B1s")
-
-	// website::tag::3::Test that the Virtual Machine size matches the Terraform specification
 	assert.Equal(t, expectedVMSize, actualVMSize)
 }
