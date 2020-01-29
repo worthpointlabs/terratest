ddc3eb76f4f50599303ced3fb874c9b60a948809
Yevgeniy Brikman <brikis98@gmail.com>
2020-01-29 11:59:54 +0000

    Clean up hello-world example

--- test/terraform_hello_world_example_test.go
+++ test/terraform_hello_world_example_test.go
@@ -2,29 +2,23 @@ package test
 
 import (
 	"testing"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-// website::tag::2::An example of how to test the simple Terraform module in examples/terraform-hello-world-example using Terratest.
 func TestTerraformHelloWorldExample(t *testing.T) {
-	t.Parallel()
-
 	terraformOptions := &terraform.Options{
-		// website::tag::2::Set the path to the Terraform code that will be tested.
+		// website::tag::1:: Set the path to the Terraform code that will be tested.
 		TerraformDir: "../examples/terraform-hello-world-example",
 	}
 
-	// website::tag::6::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
+	// website::tag::4:: Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
 
-	// website::tag::3::Run "terraform init" and "terraform apply" and fail the test if there are any errors
+	// website::tag::2:: Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// website::tag::4::Run `terraform output` to get the values of output variables
+	// website::tag::3:: Run `terraform output` to get the values of output variables and check they have the expected values.
 	output := terraform.Output(t, terraformOptions, "hello_world")
-
-	// website::tag::5::Check the output against expected values.
 	assert.Equal(t, "Hello, World!", output)
 }
