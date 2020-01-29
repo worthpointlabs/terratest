67e0c6eeb24b5dadbc43159b945bf5641764defa
Yevgeniy Brikman <brikis98@gmail.com>
2020-01-29 12:19:19 +0000

    Add packer-hello-world-example

---
+++ test/packer_hello_world_example_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/docker"
+	"github.com/gruntwork-io/terratest/modules/packer"
+	"github.com/magiconair/properties/assert"
+	"testing"
+)
+
+func TestPackerHelloWorldExample(t *testing.T) {
+	packerOptions := &packer.Options{
+		// website::tag::1:: The path to where the Packer template is located
+		Template: "../examples/packer-hello-world-example/build.json",
+	}
+
+	// website::tag::2:: Build the Packer template. This template will create a Docker image.
+	packer.BuildArtifact(t, packerOptions)
+
+	// website::tag::3:: Build the Docker image, read the text file from it, and make sure it contains the expected output.
+	opts := &docker.RunOptions{Command: []string{"cat", "/test.txt"}}
+	output := docker.Run(t, "gruntwork/packer-hello-world-example", opts)
+	assert.Equal(t, "Hello, World!", output)
+}
