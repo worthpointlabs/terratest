cbdb59a86239122cd1678b790743b42c58b00238
Nathan Sullivan <nathan@ted.com>
2020-03-25 14:03:05 +1000

    Update modules/aws/ec2-files.go
    Co-Authored-By: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- modules/aws/ec2-files.go
+++ modules/aws/ec2-files.go
@@ -92,8 +92,8 @@ func FetchContentsOfFileFromAsg(t testing.TestingT, awsRegion string, sshOpts *s
 }
 
 // FetchContentsOfFileFromAsgE looks up the EC2 Instances in the given ASG, looks up the public IPs of those EC2
-// Instances, connects to each Instance via SSH using the given username and one or more of: Key Pair, SSH Agent or
-// Override SSH Agent auth methods, fetches the contents of the file at the given path (using sudo if useSudo is true),
+// Instances, connects to each Instance via SSH (using the given username and one or more of Key Pair, SSH Agent or
+// Override SSH Agent auth methods), fetches the contents of the file at the given path (using sudo if useSudo is true),
 // and returns a map from Instance ID to the contents of that file as a string.
 func FetchContentsOfFileFromAsgE(t testing.TestingT, awsRegion string, sshOpts *ssh.Options, asgName string, useSudo bool, filePath string) (map[string]string, error) {
 	instanceIDs, err := GetInstanceIdsForAsgE(t, asgName, awsRegion)
