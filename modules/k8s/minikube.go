d05f5a1b89d9652ff987461482c927868e6ab926
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2020-04-09 07:58:08 -0700

    Update check for minikube to account for multiple minikube nodes

--- modules/k8s/minikube.go
+++ modules/k8s/minikube.go
@@ -4,33 +4,35 @@ import (
 	"strings"
 
 	"github.com/gruntwork-io/terratest/modules/testing"
+	corev1 "k8s.io/api/core/v1"
 )
 
 // IsMinikubeE returns true if the underlying kubernetes cluster is Minikube. This is determined by getting the
-// associated nodes and checking if:
-// - there is only one node
-// - the node has at least one label namespaced with "minikube.k8s.io"
+// associated nodes and checking if all nodes has at least one label namespaced with "minikube.k8s.io".
 func IsMinikubeE(t testing.TestingT, options *KubectlOptions) (bool, error) {
 	nodes, err := GetNodesE(t, options)
 	if err != nil {
 		return false, err
 	}
 
-	// ASSUMPTION: Minikube always only has one node.
-	if len(nodes) != 1 {
-		return false, nil
-	}
-
-	// ASSUMPTION: All minikube setups will have a node with labels that are namespaced with minikube.k8s.io
-	node := nodes[0]
-	labels := node.GetLabels()
-	for key, _ := range labels {
-		if strings.HasPrefix(key, "minikube.k8s.io") {
-			return true, nil
+	// ASSUMPTION: All minikube setups will have nodes with labels that are namespaced with minikube.k8s.io
+	for _, node := range nodes {
+		if !nodeHasMinikubeLabel(node) {
+			return false, nil
 		}
 	}
 
-	// At this point we know that the cluster has 1 node without the expected minikube label, so we assume it is not
-	// minikube.
-	return false, nil
+	// At this point we know that all the nodes in the cluster has the minikube label, so we return true.
+	return true, nil
+}
+
+// nodeHasMinikubeLabel returns true if any of the labels on the node is namespaced with minikube.k8s.io
+func nodeHasMinikubeLabel(node corev1.Node) bool {
+	labels := node.GetLabels()
+	for key, _ := range labels {
+		if strings.HasPrefix(key, "minikube.k8s.io") {
+			return true
+		}
+	}
+	return false
 }
