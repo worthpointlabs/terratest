9f1cb1e8d70736e655903f8b09b301a1feca813a
Stojan Nedic <stojan.nedic@reversinglabs.com>
2020-08-26 12:51:33 +0200

    Updated GetCurrentBranchName and added GetCurrentGitRef methods

--- modules/git/git.go
+++ modules/git/git.go
@@ -14,12 +14,57 @@ func GetCurrentBranchName(t testing.TestingT) string {
 	if err != nil {
 		t.Fatal(err)
 	}
+	if out == "HEAD" {
+		return ""
+	}
 	return out
 }
 
-// GetCurrentBranchNameE retrieves the current branch name.
-func GetCurrentBranchNameE(t testing.TestingT) (string, error) {
+// GetCurrentBranchNameE retrieves the current branch name. Created as variable
+// to enable mocking within the tests.
+var GetCurrentBranchNameE = func(t testing.TestingT) (string, error) {
 	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	bytes, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(bytes)), nil
+}
+
+// GetCurrentGitRef retrieves current branch name or most recent tag from a commit. If the tag
+// points to the commit, then only tag is returned.
+func GetCurrentGitRef(t testing.TestingT) string {
+	out, err := GetCurrentGitRefE(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return out
+}
+
+// GetCurrentGitRefE retrieves current branch name or most recent tag from a commit. If the tag
+// points to the commit, then only tag is returned.
+func GetCurrentGitRefE(t testing.TestingT) (string, error) {
+	out, err := GetCurrentBranchNameE(t)
+
+	if err != nil {
+		return "", err
+	}
+
+	if out != "" {
+		return out, nil
+	}
+
+	out, err = GetMostRecentTagE(t)
+	if err != nil {
+		return "", err
+	}
+	return out, nil
+}
+
+// GetMostRecentTagE retrieves most recent tag that is reachable from a commit. If the tag points
+// to the commit, then only the tag is returned. Created as variable to enable mocking within tests.
+var GetMostRecentTagE = func(t testing.TestingT) (string, error) {
+	cmd := exec.Command("git", "describe", "--tags")
 	bytes, err := cmd.Output()
 	if err != nil {
 		return "", err
--- modules/git/git_test.go
+++ modules/git/git_test.go
@@ -3,12 +3,53 @@ package git
 import (
 	"testing"
 
+	terratest_testing "github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetCurrentBranchName(t *testing.T) {
+func TestGetCurrentBranchNameReturnsBranchName(t *testing.T) {
 	t.Parallel()
 
+	GetCurrentBranchNameE = func(t terratest_testing.TestingT) (string, error) {
+		return "master", nil
+	}
 	name := GetCurrentBranchName(t)
-	assert.NotEmpty(t, name)
+
+	assert.Equal(t, name, "master")
+}
+
+func TestGetCurrentBranchNameReturnsEmptyForDetachedState(t *testing.T) {
+	t.Parallel()
+
+	GetCurrentBranchNameE = func(t terratest_testing.TestingT) (string, error) {
+		return "HEAD", nil
+	}
+
+	name := GetCurrentBranchName(t)
+	assert.Empty(t, name)
+}
+
+func TestGetCurrentRefReturnsBranchName(t *testing.T) {
+	t.Parallel()
+
+	GetCurrentBranchNameE = func(t terratest_testing.TestingT) (string, error) {
+		return "master", nil
+	}
+
+	name := GetCurrentGitRef(t)
+	assert.Equal(t, "master", name)
+}
+
+func TestGetCurrentRefReturns(t *testing.T) {
+	t.Parallel()
+
+	GetCurrentBranchNameE = func(t terratest_testing.TestingT) (string, error) {
+		return "", nil
+	}
+	GetCurrentGitRefE = func(t terratest_testing.TestingT) (string, error) {
+		return "v0.0.1", nil
+	}
+
+	name := GetCurrentGitRef(t)
+	assert.Equal(t, "v0.0.1", name)
 }
