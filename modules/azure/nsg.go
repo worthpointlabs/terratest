bf2140bdec8415c4ee1dcf963e383d23522b15b4
Mike Yeaney <miyean@microsoft.com>
2020-09-18 14:58:49 -0400

    Working through PR feedback items

--- modules/azure/nsg.go
+++ modules/azure/nsg.go
@@ -2,13 +2,18 @@ package azure
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"strings"
+	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
+	"github.com/stretchr/testify/assert"
 )
 
-// GetDefaultNsgRulesClientE returns a rules client
+// GetDefaultNsgRulesClientE returns a rules client which can be used to read the list of *default* security rules
+// defined on an network security group. Note that the "default" rules are those provided implicitly
+// by the Azure platform.
 func GetDefaultNsgRulesClientE(subscriptionID string) (network.DefaultSecurityRulesClient, error) {
 	// Validate Azure subscription ID
 	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
@@ -28,7 +33,9 @@ func GetDefaultNsgRulesClientE(subscriptionID string) (network.DefaultSecurityRu
 	return nsgClient, nil
 }
 
-// GetCustomNsgRulesClientE returns a rules client
+// GetCustomNsgRulesClientE returns a rules client which can be used to read the list of *custom* security rules
+// defined on an network security group. Note that the "custom" rules are those defined by
+// end users.
 func GetCustomNsgRulesClientE(subscriptionID string) (network.SecurityRulesClient, error) {
 	// Validate Azure subscription ID
 	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
@@ -48,42 +55,58 @@ func GetCustomNsgRulesClientE(subscriptionID string) (network.SecurityRulesClien
 	return nsgClient, nil
 }
 
-// GetAllNSGRulesE returns a slice containing all rules from the NSG (including defauls and custom)
+// GetAllNSGRulesE returns a slice containing the combined "default" and "custom" rules from a network
+// security group.
 func GetAllNSGRulesE(resourceGroupName, nsgName, subscriptionID string) (NsgRuleSummaryList, error) {
 	defaultRulesClient, err := GetDefaultNsgRulesClientE(subscriptionID)
 	if err != nil {
 		return NsgRuleSummaryList{}, err
 	}
 
+	// Get a client instance
 	customRulesClient, err := GetCustomNsgRulesClientE(subscriptionID)
 	if err != nil {
 		return NsgRuleSummaryList{}, err
 	}
 
+	// Read all default (platform) rules.
 	defaultRuleList, err := defaultRulesClient.ListComplete(context.Background(), resourceGroupName, nsgName)
 	if err != nil {
 		return NsgRuleSummaryList{}, err
 	}
 
+	// Read any custom (user provided) rules
 	customRuleList, err := customRulesClient.ListComplete(context.Background(), resourceGroupName, nsgName)
 	if err != nil {
 		return NsgRuleSummaryList{}, err
 	}
 
-	rules := make([]NsgRuleSummary, 0)
-	bindRuleList(&rules, defaultRuleList)
-	bindRuleList(&rules, customRuleList)
+	// Convert the default list to our summary type
+	boundDefaultRules, err := bindRuleList(defaultRuleList)
+	if err != nil {
+		return NsgRuleSummaryList{}, err
+	}
+
+	// Convert the custom list to our summary type
+	boundCustomRules, err := bindRuleList(customRuleList)
+	if err != nil {
+		return NsgRuleSummaryList{}, err
+	}
+
+	// Join the summarized lists and wrap in NsgRuleSummaryList struct
+	allRules := append(boundDefaultRules, boundCustomRules...)
 	ruleList := NsgRuleSummaryList{}
-	ruleList.SummarizedRules = rules
+	ruleList.SummarizedRules = allRules
 	return ruleList, nil
 }
 
-// NsgRuleSummaryList holds a colleciton of NsgRuleSummary structs
+// NsgRuleSummaryList holds a colleciton of NsgRuleSummary rules
 type NsgRuleSummaryList struct {
 	SummarizedRules []NsgRuleSummary
 }
 
-// NsgRuleSummary is a string-based summary of an NSG rule with methods attached.
+// NsgRuleSummary is a string-based (non-pointer) summary of an NSG rule with several helper methods attached
+// to help with verification of rule configuratoin.
 type NsgRuleSummary struct {
 	Name                     string
 	Description              string
@@ -97,24 +120,28 @@ type NsgRuleSummary struct {
 	Direction                string
 }
 
-func bindRuleList(dest *[]NsgRuleSummary, source network.SecurityRuleListResultIterator) {
+// bindRuleList takes a raw list of security rules from the SDK and converts them into a string-based
+// summary struct.
+func bindRuleList(source network.SecurityRuleListResultIterator) ([]NsgRuleSummary, error) {
+	rules := make([]NsgRuleSummary, 0)
 	for source.NotDone() {
 		v := source.Value()
-		*dest = append(*dest, convertToNsgRuleSummary(v.Name, v.SecurityRulePropertiesFormat))
+		rules = append(rules, convertToNsgRuleSummary(v.Name, v.SecurityRulePropertiesFormat))
 		err := source.NextWithContext(context.Background())
 		if err != nil {
-			panic(err)
+			return []NsgRuleSummary{}, err
 		}
 	}
+	return rules, nil
 }
 
+// convertToNsgRuleSummary converst the raw SDK security rule type into a summarized struct, flattening the
+// rules properties and name into a single, string-based struct.
 func convertToNsgRuleSummary(name *string, rule *network.SecurityRulePropertiesFormat) NsgRuleSummary {
 	summary := NsgRuleSummary{}
 
 	if rule.Description != nil {
 		summary.Description = *rule.Description
-	} else {
-		summary.Description = ""
 	}
 
 	summary.Name = *name
@@ -129,7 +156,7 @@ func convertToNsgRuleSummary(name *string, rule *network.SecurityRulePropertiesF
 	return summary
 }
 
-// FindRuleByName looks for a matching rule name
+// FindRuleByName looks for a matching rule by name within a collection of rules.
 func (summarizedRules *NsgRuleSummaryList) FindRuleByName(name string) NsgRuleSummary {
 	for _, r := range summarizedRules.SummarizedRules {
 		if r.Name == name {
@@ -140,55 +167,79 @@ func (summarizedRules *NsgRuleSummaryList) FindRuleByName(name string) NsgRuleSu
 	return NsgRuleSummary{}
 }
 
-// AllowsDestinationPort checks to see if the rule allows a specific destination port.
-func (summarizedRule *NsgRuleSummary) AllowsDestinationPort(port string) bool {
-	if summarizedRule.DestinationPortRange == "*" {
-		return true
-	}
-
-	low, high := ParsePortRangeString(summarizedRule.DestinationPortRange)
-	portAsInt, _ := strconv.ParseInt(port, 10, 16)
-
-	if (port == "*") && (low == 0) && (high == 65535) {
-		return true
-	}
-
-	return ((low <= uint16(portAsInt)) && (uint16(portAsInt) <= high))
+// AllowsDestinationPort checks to see if the rule allows a specific destination port. This is helpful when verifying
+// that a given rule is configured properly for a given port.
+func (summarizedRule *NsgRuleSummary) AllowsDestinationPort(t *testing.T, port string) bool {
+	allowed, err := portRangeAllowsPort(summarizedRule.DestinationPortRange, port)
+	assert.NoError(t, err)
+	return allowed
 }
 
-// AllowsSourcePort checks to see if the rule allows a specific source port.
-func (summarizedRule *NsgRuleSummary) AllowsSourcePort(port string) bool {
-	if summarizedRule.SourcePortRange == "*" {
-		return true
-	}
-
-	low, high := ParsePortRangeString(summarizedRule.SourcePortRange)
-	portAsInt, _ := strconv.ParseInt(port, 10, 16)
-
-	if (port == "*") && (low == 0) && (high == 65535) {
-		return true
-	}
-
-	return ((low <= uint16(portAsInt)) && (uint16(portAsInt) <= high))
+// AllowsSourcePort checks to see if the rule allows a specific source port. This is helpful when verifying
+// that a given rule is configured properly for a given port.
+func (summarizedRule *NsgRuleSummary) AllowsSourcePort(t *testing.T, port string) bool {
+	allowed, err := portRangeAllowsPort(summarizedRule.SourcePortRange, port)
+	assert.NoError(t, err)
+	return allowed
 }
 
-// ParsePortRangeString decodes a range string ("2-100") or a single digit ("22") and returns
+// portRangeAllowsPort is the internal impelmentation of AllowsSourcePort and AllowsDestinationPort.
+func portRangeAllowsPort(portRange string, port string) (bool, error) {
+	if portRange == "*" {
+		return true, nil
+	}
+
+	// Decode the provided port range
+	low, high, parseErr := parsePortRangeString(portRange)
+	if parseErr != nil {
+		return false, parseErr
+	}
+
+	// Decode user-provided port
+	portAsInt, parseErr := strconv.ParseInt(port, 10, 16)
+	if (parseErr != nil) && (port != "*") {
+		return false, parseErr
+	}
+
+	if (port == "*") && (low == 0) && (high == 65535) {
+		return true, nil
+	}
+
+	return ((uint16(portAsInt) >= low) && (uint16(portAsInt) <= high)), nil
+}
+
+// parsePortRangeString decodes a range string ("2-100") or a single digit ("22") and returns
 // a tuple in [low, hi] form. Note that if a single digit is supplied, both members of the
 // return tuple will be the same value (e.g., "22" returns (22, 22))
-func ParsePortRangeString(rangeString string) (low uint16, hight uint16) {
+func parsePortRangeString(rangeString string) (uint16, uint16, error) {
 	// Is this an asterisk?
 	if rangeString == "*" {
-		return uint16(0), uint16(65535)
+		return uint16(0), uint16(65535), nil
 	}
 
 	// Is this a range?
 	if !strings.Contains(rangeString, "-") {
-		val, _ := strconv.ParseInt(rangeString, 10, 16)
-		return uint16(val), uint16(val)
+		val, parseErr := strconv.ParseInt(rangeString, 10, 16)
+		if parseErr != nil {
+			return 0, 0, parseErr
+		}
+		return uint16(val), uint16(val), nil
 	}
 
 	parts := strings.Split(rangeString, "-")
-	lowVal, _ := strconv.ParseInt(parts[0], 10, 16)
-	highVal, _ := strconv.ParseInt(parts[1], 10, 16)
-	return uint16(lowVal), uint16(highVal)
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("Invalid port range specified; must be of the format '{low port}-{high port}'")
+	}
+
+	lowVal, parseErr := strconv.ParseInt(parts[0], 10, 16)
+	if parseErr != nil {
+		return 0, 0, parseErr
+	}
+
+	highVal, parseErr := strconv.ParseInt(parts[1], 10, 16)
+	if parseErr != nil {
+		return 0, 0, parseErr
+	}
+
+	return uint16(lowVal), uint16(highVal), nil
 }
