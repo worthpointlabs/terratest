ae1da94a512c86ff85a7a80fbd4684f3c974cfae
Mike Yeaney <miyean@microsoft.com>
2020-09-18 14:58:49 -0400

    Working through PR feedback items

--- modules/azure/nsg_test.go
+++ modules/azure/nsg_test.go
@@ -1,9 +1,7 @@
-// +build azure
-
 // NOTE: We use build tags to differentiate azure testing because we currently do not have azure access setup for
 // CircleCI.
 
-package test
+package azure
 
 import (
 	"fmt"
@@ -11,71 +9,70 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRangeParsingSinglePort(t *testing.T) {
-	lo, hi := azure.ParsePortRangeString("22")
+	lo, hi, _ := parsePortRangeString("22")
 	assert.Equal(t, uint16(22), lo)
 	assert.Equal(t, uint16(22), hi)
 }
 
 func TestRangeParsingPortRange(t *testing.T) {
-	lo, hi := azure.ParsePortRangeString("22-80")
+	lo, hi, _ := parsePortRangeString("22-80")
 	assert.Equal(t, uint16(22), lo)
 	assert.Equal(t, uint16(80), hi)
 }
 
 func TestRangeParsingAsterisk(t *testing.T) {
-	lo, hi := azure.ParsePortRangeString("*")
+	lo, hi, _ := parsePortRangeString("*")
 	assert.Equal(t, uint16(0), lo)
 	assert.Equal(t, uint16(65535), hi)
 }
 
 func TestRuleSummaryAllowSourcePort(t *testing.T) {
-	summary := azure.NsgRuleSummary{}
+	summary := NsgRuleSummary{}
 	summary.SourcePortRange = "22"
 
-	result := summary.AllowsSourcePort("22")
+	result := summary.AllowsSourcePort(t, "22")
 	assert.True(t, result)
 }
 
 func TestRuleSummaryAllowSourcePortAsterisk(t *testing.T) {
-	summary := azure.NsgRuleSummary{}
+	summary := NsgRuleSummary{}
 	summary.SourcePortRange = "*"
 
 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	result := summary.AllowsSourcePort(string(uint16(rand.Int())))
+	result := summary.AllowsSourcePort(t, string(uint16(rand.Int())))
 	assert.True(t, result)
 }
 
 func TestRuleSummaryAllowDestinationPort(t *testing.T) {
-	summary := azure.NsgRuleSummary{}
+	summary := NsgRuleSummary{}
 	summary.DestinationPortRange = "80"
 
-	result := summary.AllowsDestinationPort("80")
+	result := summary.AllowsDestinationPort(t, "80")
 	assert.True(t, result)
 }
 
 func TestRuleSummaryAllowDestinationPortAsterisk(t *testing.T) {
-	summary := azure.NsgRuleSummary{}
+	summary := NsgRuleSummary{}
 	summary.DestinationPortRange = "*"
 
 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	result := summary.AllowsDestinationPort(string(uint16(rand.Int())))
+	result := summary.AllowsDestinationPort(t, string(uint16(rand.Int())))
 	assert.True(t, result)
 }
 
 func TestFindSummarizedRule(t *testing.T) {
-	ruleList := azure.NsgRuleSummaryList{}
-	rules := make([]azure.NsgRuleSummary, 0)
+	ruleList := NsgRuleSummaryList{}
+	rules := make([]NsgRuleSummary, 0)
 
 	// Create some rules
 	for i := 1; i <= 10; i++ {
-		rule := azure.NsgRuleSummary{}
+		rule := NsgRuleSummary{}
 		rule.Name = fmt.Sprintf("rule_%d", i)
 		rules = append(rules, rule)
 	}
@@ -87,5 +84,5 @@ func TestFindSummarizedRule(t *testing.T) {
 
 	// Look for a rule that doesn't exist
 	match2 := ruleList.FindRuleByName("foo")
-	assert.Equal(t, azure.NsgRuleSummary{}, match2)
+	assert.Equal(t, NsgRuleSummary{}, match2)
 }
