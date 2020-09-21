07ee3c780ac80996d8a21f0c90cbbc2b849f65ca
Mike Yeaney <miyean@microsoft.com>
2020-09-21 10:35:49 -0400

    Switch tests over to table-based testing

--- modules/azure/nsg_test.go
+++ modules/azure/nsg_test.go
@@ -5,84 +5,119 @@ package azure
 
 import (
 	"fmt"
-	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestRangeParsingSinglePort(t *testing.T) {
-	lo, hi, _ := parsePortRangeString("22")
-	assert.Equal(t, uint16(22), lo)
-	assert.Equal(t, uint16(22), hi)
+func TestPortRangeParsing(t *testing.T) {
+	var cases = []struct {
+		portRange    string
+		expectedLo   int
+		expectedHi   int
+		expectsError bool
+	}{
+		{"22", 22, 22, false},
+		{"22-80", 22, 80, false},
+		{"*", 0, 65535, false},
+		{"*-*", 0, 0, true},
+		{"22-", 0, 0, true},
+		{"-80", 0, 0, true},
+		{"-", 0, 0, true},
+		{"80-22", 22, 80, false},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.portRange, func(t *testing.T) {
+			lo, hi, err := parsePortRangeString(tt.portRange)
+			if !tt.expectsError {
+				require.NoError(t, err)
+			}
+			assert.Equal(t, tt.expectedLo, int(lo))
+			assert.Equal(t, tt.expectedHi, int(hi))
+		})
+	}
 }
 
-func TestRangeParsingPortRange(t *testing.T) {
-	lo, hi, _ := parsePortRangeString("22-80")
-	assert.Equal(t, uint16(22), lo)
-	assert.Equal(t, uint16(80), hi)
+func TestAllowSourcePort(t *testing.T) {
+	var cases = []struct {
+		CaseName        string
+		SourcePortRange string
+		TestPort        string
+		Result          bool
+	}{
+		{"22 allows 22", "22", "22", true},
+		{"22 doesn't allow 80", "22", "80", false},
+		{"Any allows any", "*", "*", true},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.CaseName, func(t *testing.T) {
+			summary := NsgRuleSummary{}
+			summary.SourcePortRange = tt.SourcePortRange
+			result := summary.AllowsSourcePort(t, tt.TestPort)
+			assert.Equal(t, result, tt.Result)
+		})
+	}
 }
 
-func TestRangeParsingAsterisk(t *testing.T) {
-	lo, hi, _ := parsePortRangeString("*")
-	assert.Equal(t, uint16(0), lo)
-	assert.Equal(t, uint16(65535), hi)
-}
+func TestAllowDestinationPort(t *testing.T) {
+	var cases = []struct {
+		CaseName        string
+		SourcePortRange string
+		TestPort        string
+		Result          bool
+	}{
+		{"22 allows 22", "22", "22", true},
+		{"22 doesn't allow 80", "22", "80", false},
+		{"Any allows any", "*", "*", true},
+	}
 
-func TestRuleSummaryAllowSourcePort(t *testing.T) {
-	summary := NsgRuleSummary{}
-	summary.SourcePortRange = "22"
-
-	result := summary.AllowsSourcePort(t, "22")
-	assert.True(t, result)
-}
-
-func TestRuleSummaryAllowSourcePortAsterisk(t *testing.T) {
-	summary := NsgRuleSummary{}
-	summary.SourcePortRange = "*"
-
-	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	result := summary.AllowsSourcePort(t, string(uint16(rand.Int())))
-	assert.True(t, result)
-}
-
-func TestRuleSummaryAllowDestinationPort(t *testing.T) {
-	summary := NsgRuleSummary{}
-	summary.DestinationPortRange = "80"
-
-	result := summary.AllowsDestinationPort(t, "80")
-	assert.True(t, result)
-}
-
-func TestRuleSummaryAllowDestinationPortAsterisk(t *testing.T) {
-	summary := NsgRuleSummary{}
-	summary.DestinationPortRange = "*"
-
-	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	result := summary.AllowsDestinationPort(t, string(uint16(rand.Int())))
-	assert.True(t, result)
+	for _, tt := range cases {
+		t.Run(tt.CaseName, func(t *testing.T) {
+			summary := NsgRuleSummary{}
+			summary.DestinationPortRange = tt.SourcePortRange
+			result := summary.AllowsDestinationPort(t, tt.TestPort)
+			assert.Equal(t, result, tt.Result)
+		})
+	}
 }
 
 func TestFindSummarizedRule(t *testing.T) {
+	var cases = []struct {
+		SearchString string
+		Result       bool
+	}{
+		{"rule_1", true},
+		{"rule_2", true},
+		{"rule_3", true},
+		{"rule_4", true},
+		{"rule_5", true},
+		{"rule_6", false},
+		{"", false},
+		{"foo", false},
+	}
+
 	ruleList := NsgRuleSummaryList{}
 	rules := make([]NsgRuleSummary, 0)
 
-	// Create some rules
-	for i := 1; i <= 10; i++ {
+	// Create some base rules
+	for i := 1; i <= 5; i++ {
 		rule := NsgRuleSummary{}
 		rule.Name = fmt.Sprintf("rule_%d", i)
 		rules = append(rules, rule)
 	}
 	ruleList.SummarizedRules = rules
 
-	// Look for a rule that exists
-	match1 := ruleList.FindRuleByName("rule_5")
-	assert.Equal(t, "rule_5", match1.Name)
-
-	// Look for a rule that doesn't exist
-	match2 := ruleList.FindRuleByName("foo")
-	assert.Equal(t, NsgRuleSummary{}, match2)
+	for _, tt := range cases {
+		t.Run(tt.SearchString, func(t *testing.T) {
+			match := ruleList.FindRuleByName(tt.SearchString)
+			if tt.Result {
+				assert.Equal(t, tt.SearchString, match.Name)
+			} else {
+				assert.Equal(t, match, NsgRuleSummary{})
+			}
+		})
+	}
 }
