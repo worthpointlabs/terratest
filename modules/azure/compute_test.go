5c5c803a118b150a73036625f3bdaf208d9ced96
Dave Seepersad <davesee@microsoft.com>
2020-09-25 18:20:57 -0400

    Restored compute_test updates.

--- modules/azure/compute_test.go
+++ modules/azure/compute_test.go
@@ -17,14 +17,60 @@ If/when CRUD methods are introduced for Azure Virtual Machines, these tests can 
 (see AWS S3 tests for reference).
 */
 
-func TestGetTagsForVirtualMachineE(t *testing.T) {
+func TestGetVirtualMachineE(t *testing.T) {
 	t.Parallel()
 
 	vmName := ""
 	rgName := ""
 	subID := ""
 
-	_, err := GetTagsForVirtualMachineE(t, vmName, rgName, subID)
+	_, err := GetVirtualMachineE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineInstanceE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineInstanceE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetResourceGroupVirtualMachinesObjectsE(t *testing.T) {
+	t.Parallel()
+
+	rgName := ""
+	subID := ""
+
+	_, err := GetResourceGroupVirtualMachinesObjectsE(t, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetResourceGroupVirtualMachinesE(t *testing.T) {
+	t.Parallel()
+
+	rgName := ""
+	subID := ""
+
+	_, err := GetResourceGroupVirtualMachinesE(t, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineTagsE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineTagsE(t, vmName, rgName, subID)
 
 	require.Error(t, err)
 }
@@ -37,6 +83,126 @@ func TestGetVirtualMachineSizeE(t *testing.T) {
 	subID := ""
 
 	_, err := GetVirtualMachineSizeE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineAdminUserE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineAdminUserE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineImageE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineImageE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineAvailabilitySetIDE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineAvailabilitySetIDE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineStateE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineStateE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineOsDiskNameE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineOsDiskNameE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineManagedDiskCountE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineManagedDiskCountE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineManagedDisksE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineManagedDisksE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineNicCountE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineNicCountE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestGetVirtualMachineNicsE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := GetVirtualMachineNicsE(t, vmName, rgName, subID)
+
+	require.Error(t, err)
+}
+
+func TestVirtualMachineExistsE(t *testing.T) {
+	t.Parallel()
+
+	vmName := ""
+	rgName := ""
+	subID := ""
+
+	_, err := VirtualMachineExistsE(t, vmName, rgName, subID)
 
 	require.Error(t, err)
 }
