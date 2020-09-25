f73eedde3f246d37585cc8bf1711211ac63e84d4
Dave Seepersad <davesee@microsoft.com>
2020-09-25 12:44:01 -0400

    Included Nic Config client reflection

--- modules/azure/networkinterface.go
+++ modules/azure/networkinterface.go
@@ -2,8 +2,7 @@ package azure
 
 import (
 	"context"
-	"encoding/json"
-	"strings"
+	"reflect"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
 	"github.com/gruntwork-io/terratest/modules/testing"
@@ -28,53 +27,6 @@ func NetworkInterfaceExistsE(t testing.TestingT, nicName string, resGroupName st
 	return true, nil
 }
 
-// GetNetworkInterfacePublicIPs returns a list of all the Public IPs found in the Network Interface configurations.
-// This function would fail the test if there is an error.
-func GetNetworkInterfacePublicIPs(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) []string {
-	IPs, err := GetNetworkInterfacePublicIPsE(t, nicName, resGroupName, subscriptionID)
-	require.NoError(t, err)
-	return IPs
-}
-
-// GetNetworkInterfacePublicIPsE returns a list of all the Public IPs found in the Network Interface configurations.
-func GetNetworkInterfacePublicIPsE(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) ([]string, error) {
-	publicIPs := []string{}
-
-	// Get the Network Interface client
-	nic, err := GetNetworkInterfaceE(t, nicName, resGroupName, subscriptionID)
-	if err != nil {
-		return publicIPs, err
-	}
-
-	// Get the Public IPs from each configuration available
-	for _, IPConfiguration := range *nic.IPConfigurations {
-		if ipConfigHasPublicIP(&IPConfiguration) {
-			// Get the ID from the long string NIC representation
-			publicAddressID := GetNameFromResourceID(*IPConfiguration.PublicIPAddress.ID)
-
-			// Get the Public Ip from the Public Address client
-			publicIP := GetPublicAddressIP(t, publicAddressID, resGroupName, subscriptionID)
-			publicIPs = append(publicIPs, publicIP)
-		}
-	}
-
-	return publicIPs, nil
-}
-
-// ipConfigHasPublicIP returns true if an IP Configuration has a Public IP Address.
-// This helper method was created since a config without a public address causes a nil pointer panic
-// and the string representation is searched for the publicIPAddress text to identify it's presence.
-func ipConfigHasPublicIP(ipConfig *network.InterfaceIPConfiguration) bool {
-	var byteIPConfig []byte
-
-	byteIPConfig, err := json.Marshal(ipConfig)
-	if err != nil {
-		return false
-	}
-
-	return strings.Contains(string(byteIPConfig), "publicIPAddress")
-}
-
 // GetNetworkInterfacePrivateIPs gets a list of the Private IPs of a Network Interface configs.
 // This function would fail the test if there is an error.
 func GetNetworkInterfacePrivateIPs(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) []string {
@@ -86,7 +38,7 @@ func GetNetworkInterfacePrivateIPs(t testing.TestingT, nicName string, resGroupN
 
 // GetNetworkInterfacePrivateIPsE gets a list of the Private IPs of a Network Interface configs.
 func GetNetworkInterfacePrivateIPsE(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) ([]string, error) {
-	privateIPs := []string{}
+	var privateIPs []string
 
 	// Get the Network Interface client
 	nic, err := GetNetworkInterfaceE(t, nicName, resGroupName, subscriptionID)
@@ -100,6 +52,87 @@ func GetNetworkInterfacePrivateIPsE(t testing.TestingT, nicName string, resGroup
 	}
 
 	return privateIPs, nil
+}
+
+// GetNetworkInterfacePublicIPs returns a list of all the Public IPs found in the Network Interface configurations.
+// This function would fail the test if there is an error.
+func GetNetworkInterfacePublicIPs(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) []string {
+	IPs, err := GetNetworkInterfacePublicIPsE(t, nicName, resGroupName, subscriptionID)
+	require.NoError(t, err)
+	return IPs
+}
+
+// GetNetworkInterfacePublicIPsE returns a list of all the Public IPs found in the Network Interface configurations.
+func GetNetworkInterfacePublicIPsE(t testing.TestingT, nicName string, resGroupName string, subscriptionID string) ([]string, error) {
+	var publicIPs []string
+
+	// Get the Network Interface client
+	nic, err := GetNetworkInterfaceE(t, nicName, resGroupName, subscriptionID)
+	if err != nil {
+		return publicIPs, err
+	}
+
+	// Get the Public IPs from each configuration available
+	for _, IPConfiguration := range *nic.IPConfigurations {
+		// Iterate each config, for successful configurations check for a Public Address reference.
+		// Not failing on errors as as this is an optimistic accumulator.
+		nicConfig, err := GetNetworkInterfaceConfigurationE(t, nicName, *IPConfiguration.Name, resGroupName, subscriptionID)
+		if err == nil {
+			if publicAddressID := nicConfig.PublicIPAddress; !reflect.ValueOf(nicConfig.PublicIPAddress).IsNil() {
+				// Get Public Address IP for configs with using Public Address client
+				publicIP, err := GetPublicAddressIPE(t, GetNameFromResourceID(*publicAddressID.ID), resGroupName, subscriptionID)
+				if err == nil {
+					publicIPs = append(publicIPs, publicIP)
+				}
+			}
+		}
+	}
+
+	return publicIPs, nil
+}
+
+// GetNetworkInterfaceConfigurationE gets a Network Interface Configuration in the specified Azure Resource Group.
+func GetNetworkInterfaceConfigurationE(t testing.TestingT, nicName string, nicConfigName string, resGroupName string, subscriptionID string) (*network.InterfaceIPConfiguration, error) {
+	// Validate Azure Resource Group
+	resGroupName, err := getTargetAzureResourceGroupName(resGroupName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the client reference
+	client, err := GetNetworkInterfaceConfigurationClientE(subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the Network Interface
+	nicConfig, err := client.Get(context.Background(), resGroupName, nicName, nicConfigName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &nicConfig, nil
+}
+
+// GetNetworkInterfaceConfigurationClientE creates a new Network Interface Configuration client in the specified Azure Subscription.
+func GetNetworkInterfaceConfigurationClientE(subscriptionID string) (*network.InterfaceIPConfigurationsClient, error) {
+	// Validate Azure Subscription ID
+	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the NIC client
+	client := network.NewInterfaceIPConfigurationsClient(subscriptionID)
+
+	// Create an authorizer
+	authorizer, err := NewAuthorizer()
+	if err != nil {
+		return nil, err
+	}
+	client.Authorizer = *authorizer
+
+	return &client, nil
 }
 
 // GetNetworkInterfaceE gets a Network Interface in the specified Azure Resource Group.
