0a6cb453e7f4c376805c0d0f7648fcd874d637d6
David Khourshid <davidkpiano@gmail.com>
2020-10-01 17:58:10 -0400

    Remove comment

--- test/azure/disk_test.go
+++ test/azure/disk_test.go
@@ -12,12 +12,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-/*
-The below tests are currently stubbed out, with the expectation that they will throw errors.
-If/when CRUD methods are introduced for Azure Virtual Machines, these tests can be extended
-(see AWS S3 tests for reference).
-*/
-
 func TestGetDiskE(t *testing.T) {
 	t.Parallel()
 
