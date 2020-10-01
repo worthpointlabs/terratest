7604e4f362fef3c34ad0bd6ef3e31727a4d31e4d
rho song <rhomsang@gmail.com>
2020-10-01 15:10:42 -0700

    Create a test for RunDockerComposeAndGetStdout

---
+++ test/docker_stdout_example_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/docker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDockerComposeStdoutExample(t *testing.T) {
+	dockerComposeFile := "../examples/docker-compose-stdout-example/docker-compose.yml"
+
+	// Build the Docker image.
+	docker.RunDockerCompose(
+		t,
+		&docker.Options{},
+		"-f",
+		dockerComposeFile,
+		"build",
+	)
+
+	// Run the Docker image, read the text file from it, and make sure it contains the expected output.
+	output := docker.RunDockerComposeAndGetStdOut(
+		t,
+		&docker.Options{},
+		"-f",
+		dockerComposeFile,
+		"up",
+	)
+
+	assert.Contains(t, output, "stdout: message")
+	assert.NotContains(t, output, "stderr: error")
+}
