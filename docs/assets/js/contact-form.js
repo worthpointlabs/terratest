700d3949be1f49628a0bf03ad5e320460b338660
Arkadiusz Celer <arku@altalogy.com>
2020-10-20 21:01:44 +0200

    change submit from form to ajax

--- docs/assets/js/contact-form.js
+++ docs/assets/js/contact-form.js
@@ -3,47 +3,85 @@
 $(function() {
   var submitButton = $("#submit-button");
   var form = $("#contact-form");
-
-  setRadioSelectFromUrl();
- 
-  function setRadioSelectFromUrl() {
+  
+  selectPlanFromUrl();
+  
+  submitButton.on("click", submitForm);
+  
+  function selectPlanFromUrl() {
     var params = window.location.search;
     if (params === "?plan=enterprise") {
       var plan = $("#enterprise");
-      plan.prop('checked',true)
+      plan.prop('checked', true);
     } else {
       var plan = $("#pro");
-      plan.prop('checked',true)
+      plan.prop('checked', true);
     }
   }
 
-  function submitForm() {
+  function submitForm(e) {
+    e.preventDefault();
 
-    clearErrors();
-  
-    form.find("[required]").each(function(index, el) {
-      if (!$(el).val()) {
-        isValid = false;
-        showInputError(el);
-        showFormError("Please fill in all required fields");
-      }
-    });
-  
+    if(validateForm()) {
+      var data = form.serialize(form.get(0), { hash: true });
+      submitToFormSpree(data)
+    }
+
+    function submitToFormSpree(data) {
+      submitButton.html("Sending...");
+      submitButton.prop("disabled", true);
+
+      var postParams = {
+        url: 'https://api.formbucket.com/f/buk_eldvLHDisTrizpsFPDfOm7fY',
+        type: "POST",
+        data: data,
+        dataType: "json"
+      };
+
+      $.ajax(postParams)
+        .done(function() {
+          inCall = false;
+          window.location.replace("/thanks");
+        })
+        .fail(function() {
+          showFormError(
+            "Oops, something went wrong! Please try again. If the issue persists please email us directly at info@gruntwork.io"
+          );
+          inCall = false;
+          submitButton.html("Submit");
+          submitButton.prop("disabled", false);
+        });
+    }
+    
     function showInputError(el) {
       $(el).addClass("has-error");
     };
-  
+    
     function showFormError(message) {
       $("#error-message").html(
         '<h3 class="text-danger text-center">' + message + "</h3>"
-      );
-    };
-  
-    function clearErrors() {
-      $("#error-message").html("");
-      form.find("*").removeClass("has-error");
-    };
-  }
+        );
+      };
+      
+      function clearErrors() {
+        $("#error-message").html("");
+        form.find("*").removeClass("has-error");
+      };
 
-  submitButton.on("click", submitForm);
-});
+     function validateForm() {
+        var isValid = true;
+    
+        clearErrors();
+    
+        form.find("[required]").each(function(index, el) {
+          if (!$(el).val()) {
+            isValid = false;
+            showInputError(el);
+            showFormError("Please fill in all required fields");
+          }
+        });
+    
+        return isValid;
+      };
+    }
+  });
