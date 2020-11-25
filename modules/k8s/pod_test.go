84fbf41de653999d17a261679a381fe9e9ccd41f
tu-antoine <antoine@cybozu.co.jp>
2020-11-25 08:46:42 +0000

    add test cases for IsPodAvailable

--- modules/k8s/pod_test.go
+++ modules/k8s/pod_test.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/gruntwork-io/terratest/modules/random"
@@ -94,6 +95,31 @@ func TestWaitUntilPodWithMultipleContainersAvailableReturnsSuccessfully(t *testi
 	WaitUntilPodAvailable(t, options, "nginx-pod", 60, 1*time.Second)
 }
 
+func TestWaitUntilPodAvailableWithReadinessProbe(t *testing.T) {
+	t.Parallel()
+
+	uniqueID := strings.ToLower(random.UniqueId())
+	options := NewKubectlOptions("", "", uniqueID)
+	configData := fmt.Sprintf(EXAMPLE_POD_WITH_READINESS_PROBE, uniqueID, uniqueID)
+	defer KubectlDeleteFromString(t, options, configData)
+	KubectlApplyFromString(t, options, configData)
+
+	WaitUntilPodAvailable(t, options, "nginx-pod", 60, 1*time.Second)
+}
+
+func TestWaitUntilPodAvailableWithFailingReadinessProbe(t *testing.T) {
+	t.Parallel()
+
+	uniqueID := strings.ToLower(random.UniqueId())
+	options := NewKubectlOptions("", "", uniqueID)
+	configData := fmt.Sprintf(EXAMPLE_POD_WITH_READINESS_PROBE, uniqueID, uniqueID)
+	defer KubectlDeleteFromString(t, options, configData)
+	KubectlApplyFromString(t, options, configData)
+
+	err := WaitUntilPodAvailableE(t, options, "nginx-pod", 60, 1*time.Second)
+	require.Error(t, err)
+}
+
 const EXAMPLE_POD_YAML_TEMPLATE = `---
 apiVersion: v1
 kind: Namespace
@@ -119,3 +145,69 @@ const EXAMPLE_POD_WITH_MULTIPLE_CONTAINERS_YAML_TEMPLATE = EXAMPLE_POD_YAML_TEMP
     ports:
     - containerPort: 80
 `
+
+const EXAMPLE_POD_WITH_READINESS_PROBE = EXAMPLE_POD_YAML_TEMPLATE + `
+    readinessProbe:
+      httpGet:
+        path: /
+        port: 80
+`
+
+const EXAMPLE_POD_WITH_FAILING_READINESS_PROBE = EXAMPLE_POD_YAML_TEMPLATE + `
+    readinessProbe:
+      httpGet:
+        path: /not-ready
+        port: 80
+      periodSeconds: 1
+`
+
+func TestIsPodAvailable(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct{
+		title          string
+		pod            *corev1.Pod
+		expectedResult bool
+	}{
+		{
+			title: "TestIsPodAvailableStartedButNotReady",
+			pod: &corev1.Pod{
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "container1",
+							Ready: false,
+							Started: &[]bool{true}[0],
+						},
+					},
+					Phase: corev1.PodRunning,
+				},
+			},
+			expectedResult: false,
+		},
+		{
+			title: "TestIsPodAvailableStartedAndReady",
+			pod: &corev1.Pod{
+				Status: corev1.PodStatus{
+					ContainerStatuses: []corev1.ContainerStatus{
+						{
+							Name: "container1",
+							Ready: true,
+							Started: &[]bool{true}[0],
+						},
+					},
+					Phase: corev1.PodRunning,
+				},
+			},
+			expectedResult: true,
+		},
+	}
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			t.Parallel()
+			actualResult := IsPodAvailable(tc.pod)
+			require.Equal(t, tc.expectedResult, actualResult)
+		})
+	}
+}
