88513c2636996b4d0ff6465324cd0ae3464249fe
JD Stuart <jdirkstuart@gmail.com>
2020-11-30 12:32:57 -0800

    Add support for SSM Parameter deletion (#687)
    * Add support for SSM Parameter deletion
    
    * Update ssm_test.go
    
    Co-authored-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- modules/aws/ssm_test.go
+++ modules/aws/ssm_test.go
@@ -21,5 +21,20 @@ func TestParameterIsFound(t *testing.T) {
 	keyValue := GetParameter(t, awsRegion, expectedName)
 	logger.Logf(t, "Found key with name %s", expectedName)
 	assert.Equal(t, expectedValue, keyValue)
+}
 
+func TestParameterIsDeleted(t *testing.T) {
+	expectedName := fmt.Sprintf("test-name-%s", random.UniqueId())
+	awsRegion := GetRandomRegion(t, nil, nil)
+	expectedValue := fmt.Sprintf("test-value-%s", random.UniqueId())
+	expectedDescription := fmt.Sprintf("test-description-%s", random.UniqueId())
+	version := PutParameter(t, awsRegion, expectedName, expectedDescription, expectedValue)
+	logger.Logf(t, "Created parameter with version %d", version)
+
+	DeleteParameter(t, awsRegion, expectedName)
+	logger.Logf(t, "Deleted paramter %s", expectedName)
+
+	actualValue, err := GetParameterE(t, awsRegion, expectedName)
+	assert.Equal(t, actualValue, "")
+	assert.Error(t, err)
 }
