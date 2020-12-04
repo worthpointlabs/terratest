9e2e9e3339aef753839f54558c9480c3326008b6
Yevgeniy Brikman <brikis98@gmail.com>
2020-12-04 11:23:43 +0000

    Fix formatting on Terraform files

--- examples/azure/terraform-azure-sqldb-example/variables.tf
+++ examples/azure/terraform-azure-sqldb-example/variables.tf
@@ -18,19 +18,19 @@
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable location {
+variable "location" {
   description = "The supported azure location where the resource exists"
   type        = string
   default     = "West US2"
 }
 
-variable sqlserver_admin_login {
+variable "sqlserver_admin_login" {
   description = "The administrator login name for the sql server."
   type        = string
   default     = "AdminUser2314"
 }
 
-variable tags {
+variable "tags" {
   description = "A mapping of tags to assign to the resource."
   type        = string
   default     = "Development"
