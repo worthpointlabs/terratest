f159307138b37357f64c0bdbb2da1f7e7c8aaa03
Rob Morgan <robbym@gmail.com>
2020-12-08 18:59:54 +0800

    ensure the terraform aws feature can pass

--- features/terraform_aws_example.feature
+++ features/terraform_aws_example.feature
@@ -5,4 +5,4 @@ Feature: Terraform AWS Example
     And an input variable named "instance_name" with the value "test-instance-1"
     And an environment variable named "AWS_DEFAULT_REGION" with the value "us-east-1"
     When I run "terraform apply"
-    Then the "instance_id" output is "Hello, World!"
+    Then the "instance_id" output should match "i-[0-9a-z]+(\,i-[0-9a-z]+)*"
--- modules/tspec/keyword_handler.go
+++ modules/tspec/keyword_handler.go
@@ -14,28 +14,7 @@ var TerraformOptions *terraform.Options
 
 func InitializeTestSuite(ctx *TestSuiteContext) {
 	ctx.BeforeSuite(func() {
-    // Initialize new Terraform Options
-    retryableErrors := map[string]string{
-  		"diffs didn't match during apply": "This usually indicates a minor Terraform timing bug (https://github.com/hashicorp/terraform/issues/5200) that goes away when you reapply. Retrying terraform apply.",
-
-  		// `terraform init` frequently fails in CI due to network issues accessing plugins. The reason is unknown, but
-  		// eventually these succeed after a few retries.
-  		".*unable to verify signature.*":             "Failed to retrieve plugin due to transient network error.",
-  		".*unable to verify checksum.*":              "Failed to retrieve plugin due to transient network error.",
-  		".*no provider exists with the given name.*": "Failed to retrieve plugin due to transient network error.",
-  		".*registry service is unreachable.*":        "Failed to retrieve plugin due to transient network error.",
-  	}
-
-    TerraformOptions = &terraform.Options{
-      TerraformDir:             "",
-  		RetryableTerraformErrors: retryableErrors,
-
-			// Variables to pass to our Terraform code using -var options
-			Vars: map[string]interface{}{},
-
-			// Environment variables to set when running Terraform
-			EnvVars: map[string]string{},
-    }
+    // Do nothing for now
   })
 }
 
@@ -44,7 +23,33 @@ func InitializeScenario(ctx *ScenarioContext) {
 	ctx.Step(`^an input variable named "([^"]*)" with the value "([^"]*)"$`, terraformInputVarHandler)
 	ctx.Step(`^an environment variable named "([^"]*)" with the value "([^"]*)"$`, terraformEnvVarHandler)
 	ctx.Step(`^I run "([^"]*)"$`, whenIRunHandler)
-	ctx.Step(`^the "([^"]*)" output is "([^"]*)"$`, assertOutputIsHandler)
+	ctx.Step(`^the "([^"]*)" output is "([^"]*)"$`, assertOutputEqualsHandler)
+	ctx.Step(`^the "([^"]*)" output should match "([^"]*)"$`, assertOutputMatchesHandler)
+
+	// Initialize new Terraform Options
+	// TODO - this may or may not have parallelism issues. We need to test that scenarios don't share the global TerraformOptions.
+	retryableErrors := map[string]string{
+		"diffs didn't match during apply": "This usually indicates a minor Terraform timing bug (https://github.com/hashicorp/terraform/issues/5200) that goes away when you reapply. Retrying terraform apply.",
+
+		// `terraform init` frequently fails in CI due to network issues accessing plugins. The reason is unknown, but
+		// eventually these succeed after a few retries.
+		".*unable to verify signature.*":             "Failed to retrieve plugin due to transient network error.",
+		".*unable to verify checksum.*":              "Failed to retrieve plugin due to transient network error.",
+		".*no provider exists with the given name.*": "Failed to retrieve plugin due to transient network error.",
+		".*registry service is unreachable.*":        "Failed to retrieve plugin due to transient network error.",
+	}
+
+	TerraformOptions = &terraform.Options{
+		TerraformDir:             "",
+		RetryableTerraformErrors: retryableErrors,
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{},
+
+		// Environment variables to set when running Terraform
+		EnvVars: map[string]string{},
+	}
+
 	// TODO - run Terraform destroy automatically
 }
 
@@ -80,12 +85,21 @@ func whenIRunHandler(cmd string) error {
 	}
 }
 
-func assertOutputIsHandler(outputVar, expected string) error {
+func assertOutputEqualsHandler(outputVar, expected string) error {
 	innerT := &testing.T{}
 	output := terraform.Output(innerT, TerraformOptions, outputVar)
 	return assertExpectedAndActual(
 		assert.Equal, expected, output,
 		"Expected %s output to be %s", outputVar, expected,
+	)
+}
+
+func assertOutputMatchesHandler(outputVar, expectedMatchRegex string) error {
+	innerT := &testing.T{}
+	output := terraform.Output(innerT, TerraformOptions, outputVar)
+	return assertExpectedAndActual(
+		assert.Regexp, expectedMatchRegex, output,
+		"Expected %s output to match the regex %s", outputVar, expectedMatchRegex,
 	)
 }
 
