6a56df07ed04ea4b5ec977f10246400859775e71
The Skunk Lab <theskunklab@gmail.com>
2021-01-21 15:22:23 -0600

    feat: adding Azure KeyVault module (#636)
    * keyvault module
    
    * kv files
    
    * kv update
    
    * update readme
    
    * prefix
    
    * move keyvault unit test
    
    * kv update tests
    
    * chg package
    
    * Fix the package ref to azure now that test is in same package.
    
    * adding updated go.sum
    
    * remove azure ref
    
    * refactor test
    
    * refactor test
    
    * naming
    
    * keyvault module
    
    * kv files
    
    * kv update
    
    * update readme
    
    * prefix
    
    * move keyvault unit test
    
    * kv update tests
    
    * chg package
    
    * Fix the package ref to azure now that test is in same package.
    
    * refactor test
    
    * refactor test
    
    * naming
    
    * best prac chg
    
    * module func E comments
    
    * comments
    
    * add comment
    
    * rm space
    
    * minor nits
    
    * per comments
    
    * remove resource_group_basename
    
    * remove commented out Vars in test.
    
    * chg ex test + main.tf comment
    
    * rm adding pkg
    
    * minor doc edit.
    
    * Moved key vault DNS suffix lookup into client_factory to leverage the cloud environment lookup support
    
    * update to match keyvault enabled Soft-delete flag
    
    * add Getkeyvault
    
    * fix tf format
    
    * update to support non public cloud
    
    * address pr comments
    
    * turn off soft delete
    
    * address pr comments
    
    * resolve pr comments
    
    Co-authored-by: wmattlong <malong@microsoft.com>
    Co-authored-by: richard guthrie <richardguthrie@MININT-VULFH84.fareast.corp.microsoft.com>
    Co-authored-by: richard guthrie <richardguthrie@MacBook-Pro.local>
    Co-authored-by: Richard Guthrie <rguthrie@microsoft.com>
    Co-authored-by: Mike Yeaney <miyean@microsoft.com>
    Co-authored-by: Hadwa Abdelhalem <hadwaa@microsoft.com>

--- modules/azure/client_factory.go
+++ modules/azure/client_factory.go
@@ -17,6 +17,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/profiles/preview/cosmos-db/mgmt/documentdb"
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2019-11-01/containerservice"
+	kvmng "github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2016-10-01/keyvault"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-06-01/subscriptions"
 	autorestAzure "github.com/Azure/go-autorest/autorest/azure"
 )
@@ -135,6 +136,38 @@ func CreateCosmosDBSQLClientE(subscriptionID string) (*documentdb.SQLResourcesCl
 	cosmosClient := documentdb.NewSQLResourcesClientWithBaseURI(baseURI, subscriptionID)
 
 	return &cosmosClient, nil
+}
+
+// CreateKeyVaultManagementClientE is a helper function that will setup a key vault management client with the correct BaseURI depending on
+// the Azure environment that is currently setup (or "Public", if none is setup).
+func CreateKeyVaultManagementClientE(subscriptionID string) (*kvmng.VaultsClient, error) {
+	// Validate Azure subscription ID
+	subscriptionID, err := getTargetAzureSubscription(subscriptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Lookup environment URI
+	baseURI, err := getEnvironmentEndpointE(ResourceManagerEndpointName)
+	if err != nil {
+		return nil, err
+	}
+
+	//create keyvault management clinet
+	vaultClient := kvmng.NewVaultsClientWithBaseURI(baseURI, subscriptionID)
+
+	return &vaultClient, nil
+}
+
+// GetKeyVaultURISuffixE returns the proper KeyVault URI suffix for the configured Azure environment.
+// This function would fail the test if there is an error.
+func GetKeyVaultURISuffixE() (string, error) {
+	envName := getDefaultEnvironmentName()
+	env, err := autorestAzure.EnvironmentFromName(envName)
+	if err != nil {
+		return "", err
+	}
+	return env.KeyVaultDNSSuffix, nil
 }
 
 // getDefaultEnvironmentName returns either a configured Azure environment name, or the public default
