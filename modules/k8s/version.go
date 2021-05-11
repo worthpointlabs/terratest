80f6c94edb05e4db87d41521773f81cda6940014
Fares Almotlag <f.almotlag@gmail.com>
2021-05-12 02:30:23 +0300

    Add a version function that takes options

--- modules/k8s/version.go
+++ modules/k8s/version.go
@@ -4,7 +4,19 @@ import "github.com/gruntwork-io/terratest/modules/testing"
 
 // GetKubernetesClusterVersion returns the Kubernetes cluster version.
 func GetKubernetesClusterVersionE(t testing.TestingT) (string, error) {
-	clientset, err := GetKubernetesClientE(t)
+	kubeConfigPath, err := GetKubeConfigPathE(t)
+	if err != nil {
+		return "", err
+	}
+
+	options := NewKubectlOptions("", kubeConfigPath, "default")
+
+	return GetKubernetesClusterVersionWithOptionsE(t, options)
+}
+
+// GetKubernetesClusterVersion returns the Kubernetes cluster version given a configured KubectlOptions object.
+func GetKubernetesClusterVersionWithOptionsE(t testing.TestingT, kubectlOptions *KubectlOptions) (string, error) {
+	clientset, err := GetKubernetesClientFromOptionsE(t, kubectlOptions)
 	if err != nil {
 		return "", err
 	}
