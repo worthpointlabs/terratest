2799cb16ce114b706edf3246db93c8732d59553a
Zachary Proser <zackproser@gmail.com>
2021-06-10 15:35:09 -0400

    Don't return error

--- modules/terraform/validate.go
+++ modules/terraform/validate.go
@@ -1,19 +1,24 @@
 package terraform
 
 import (
+	// We alias Golang's native testing package to go_test to avoid naming conflicts with terratest's own testing module
 	go_test "testing"
 
 	"github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/stretchr/testify/require"
 )
 
-// Automatically finds all folders specified in RootDir that contain .tf files and runs InitAndValidate in all of them
-// Excludes any folders specified in the ExcludeDirs slice
-func ValidateAllTerraformModules(t *go_test.T, opts ValidationOptions) error {
+// ValidateAllTerraformModules rutomatically finds all folders specified in RootDir that contain .tf files and runs
+// InitAndValidate in all of them.
+// Filters down to only those paths passed in ValidationOptions.IncludeDirs, if passed.
+// Excludes any folders specified in the ValidationOptions.ExcludeDirs. IncludeDirs will take precedence over ExcludeDirs
+// Use the NewValidationOptions method to pass relative paths for either of these options to have the full paths built
+// Note that go_test is an alias to Golang's native testing package created to avoid naming conflicts with Terratest's
+// own testing package
+func ValidateAllTerraformModules(t *go_test.T, opts ValidationOptions) {
 	dirsToValidate, readErr := FindTerraformModulePathsInRootE(opts)
-	if readErr != nil {
-		return readErr
-	}
+	require.NoError(t, readErr)
+
 	for _, dir := range dirsToValidate {
 		dir := dir
 		t.Run(dir, func(t *go_test.T) {
@@ -22,7 +27,6 @@ func ValidateAllTerraformModules(t *go_test.T, opts ValidationOptions) error {
 			InitAndValidate(t, tfOpts)
 		})
 	}
-	return nil
 }
 
 // Validate calls terraform validate and returns stdout/stderr.
