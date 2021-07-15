348ae77e0d25aab667d8bb7c9e064d1d9d84308b
Hadwa Abdelhalem <hadwaa@microsoft.com>
2021-07-15 12:03:52 -0700

    update azurefunction test

--- test/azure/terraform_azure_functionapp_example_test.go
+++ test/azure/terraform_azure_functionapp_example_test.go
@@ -6,9 +6,7 @@
 package test
 
 import (
-	"fmt"
 	"strings"
-
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
@@ -20,32 +18,38 @@ import (
 func TestTerraformAzureFunctionAppExample(t *testing.T) {
 	t.Parallel()
 
-	_random := strings.ToLower(random.UniqueId())
+	//_random := strings.ToLower(random.UniqueId())
+	uniquePostfix := strings.ToLower(random.UniqueId())
 
-	expectedKind := "functionapp"
-	expectedResourceName := fmt.Sprintf("%s-func", _random)
-	expectedResourceGroupName := fmt.Sprintf("%s-rg", _random)
-
+	// website::tag::1:: Configure Terraform setting up a path to Terraform code.
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../../examples/azure/terraform-azure-functionapp-example",
 		Vars: map[string]interface{}{
-			"location":     "Central US",
-			"project_name": _random,
+			"postfix": uniquePostfix,
 		},
 	}
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	// website::tag::5:: At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	// website::tag::2:: Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	site := azure.GetFunctionApp(t, expectedResourceName, expectedResourceGroupName, "")
+	// website::tag::3:: Run `terraform output` to get the values of output variables
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	appName := terraform.Output(t, terraformOptions, "function_app_name")
 
-	assert := assert.New(t)
+	appId := terraform.Output(t, terraformOptions, "function_app_id")
+	appDefaultHostName := terraform.Output(t, terraformOptions, "default_hostname")
+	appKind := terraform.Output(t, terraformOptions, "function_app_kind")
 
-	assert.NotNil(*site)
+	// website::tag::4:: Assert
+	assert.True(t, azure.AppExists(t, appName, resourceGroupName, ""))
+	site := azure.GetAppService(t, appName, resourceGroupName, "")
 
-	assert.Equal(expectedKind, *site.Kind)
+	assert.Equal(t, appId, *site.ID)
+	assert.Equal(t, appDefaultHostName, *site.DefaultHostName)
+	assert.Equal(t, appKind, *site.Kind)
 
-	assert.NotEmpty(*site.OutboundIPAddresses)
+	assert.NotEmpty(t, *site.OutboundIPAddresses)
+	assert.Equal(t, "Running", *site.State)
 }
