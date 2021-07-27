cb83f8dd17b4ee6a4063b270259d96b7f0317f16
Hadwa Abdelhalem <hadwaa@microsoft.com>
2021-07-27 11:44:13 -0700

    fix tf format

--- examples/azure/terraform-azure-acr-example/output.tf
+++ examples/azure/terraform-azure-acr-example/output.tf
@@ -2,7 +2,7 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "container_registry_name"{
+output "container_registry_name" {
   value = azurerm_container_registry.acr.name
 }
 
