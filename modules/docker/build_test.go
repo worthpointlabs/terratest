2c215cfb0fb1d19d1a26e094249bf178b2851b28
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2021-08-07 08:01:21 -0500

    Support multiarch docker image builds (#970)
    * Support multiarch docker image builds
    
    * Make sure to install docker buildx plugin in circleci
    
    * Use newer ubuntu and docker for buildx
    
    * Remove test
    
    * Fix build
    
    * Make sure to check against the right name
    
    * Actually fix ecr repo tests

--- modules/docker/build_test.go
+++ modules/docker/build_test.go
@@ -24,6 +24,24 @@ func TestBuild(t *testing.T) {
 	require.Contains(t, out, text)
 }
 
+func TestBuildMultiArch(t *testing.T) {
+	t.Parallel()
+
+	tag := "gruntwork-io/test-image:v1"
+	text := "Hello, World!"
+
+	options := &BuildOptions{
+		Tags:          []string{tag},
+		BuildArgs:     []string{fmt.Sprintf("text=%s", text)},
+		Architectures: []string{"linux/arm64", "linux/amd64"},
+		Load:          true,
+	}
+
+	Build(t, "../../test/fixtures/docker", options)
+	out := Run(t, tag, &RunOptions{Remove: true})
+	require.Contains(t, out, text)
+}
+
 func TestBuildWithTarget(t *testing.T) {
 	t.Parallel()
 
