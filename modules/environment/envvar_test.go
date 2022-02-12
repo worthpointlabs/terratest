08558f5bb69fcc9a09dab3c3c73fcbb624c9764e
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-02-11 21:05:00 -0600

    Add function to require an env var is defined in test (#1061)

--- modules/environment/envvar_test.go
+++ modules/environment/envvar_test.go
@@ -7,6 +7,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// MockT is used to test that the function under test will fail the test under certain circumstances.
+type MockT struct {
+	Failed bool
+}
+
+func (t *MockT) Fail() {
+	t.Failed = true
+}
+
+func (t *MockT) FailNow() {
+	t.Failed = true
+}
+
+func (t *MockT) Error(args ...interface{}) {
+	t.Failed = true
+}
+
+func (t *MockT) Errorf(format string, args ...interface{}) {
+	t.Failed = true
+}
+
+func (t *MockT) Fatal(args ...interface{}) {
+	t.Failed = true
+}
+
+func (t *MockT) Fatalf(format string, args ...interface{}) {
+	t.Failed = true
+}
+
+func (t *MockT) Name() string {
+	return "mockT"
+}
+
+// End MockT
+
 var envvarList = []string{
 	"TERRATEST_TEST_ENVIRONMENT",
 	"TERRATESTTESTENVIRONMENT",
@@ -31,4 +66,28 @@ func TestGetFirstNonEmptyEnvVarOrEmptyStringReturnsEmpty(t *testing.T) {
 
 	value := GetFirstNonEmptyEnvVarOrEmptyString(t, envvarList)
 	assert.Equal(t, value, "")
+}
+
+func TestRequireEnvVarFails(t *testing.T) {
+	// These tests can not run in parallel, since they manipulate env vars
+	// DO NOT ADD THIS: t.Parallel()
+
+	envVarName := "TERRATESTTESTENVIRONMENT"
+	mockT := new(MockT)
+
+	// Make sure the check fails when env var is not set
+	RequireEnvVar(mockT, envVarName)
+	assert.True(t, mockT.Failed)
+}
+
+func TestRequireEnvVarPasses(t *testing.T) {
+	// These tests can not run in parallel, since they manipulate env vars
+	// DO NOT ADD THIS: t.Parallel()
+
+	envVarName := "TERRATESTTESTENVIRONMENT"
+
+	// Make sure the check passes when env var is set
+	os.Setenv(envVarName, "test")
+	defer os.Setenv(envVarName, "")
+	RequireEnvVar(t, envVarName)
 }
