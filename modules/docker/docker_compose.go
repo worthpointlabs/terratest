ffe636533c8cc519d582a61f83e7239a4a033a85
nialdaly <nial.daly@outlook.com>
2022-06-19 19:44:27 +0100

    refactored logic for Docker Compose backwards compat handling

--- modules/docker/docker_compose.go
+++ modules/docker/docker_compose.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/shell"
 	"github.com/gruntwork-io/terratest/modules/testing"
 	"github.com/stretchr/testify/require"
+	"gotest.tools/v3/icmd"
 )
 
 // Options are Docker options.
@@ -40,29 +41,23 @@ func RunDockerComposeE(t testing.TestingT, options *Options, args ...string) (st
 
 func runDockerComposeE(t testing.TestingT, stdout bool, options *Options, args ...string) (string, error) {
 	cmd := shell.Command{
-		Command: "docker",
+		Command: "docker-compose",
 		// We append --project-name to ensure containers from multiple different tests using Docker Compose don't end
 		// up in the same project and end up conflicting with each other.
-		Args:       append([]string{"compose", "--project-name", strings.ToLower(t.Name())}, args...),
+		Args:       append([]string{"--project-name", strings.ToLower(t.Name())}, args...),
 		WorkingDir: options.WorkingDir,
 		Env:        options.EnvVars,
 		Logger:     options.Logger,
 	}
 
-	checkDockerComposeVersionCmd := shell.Command{
-		Command:    "docker",
-		Args:       []string{"compose", "version"},
-		WorkingDir: options.WorkingDir,
-		Env:        options.EnvVars,
-		Logger:     options.Logger,
-	}
+	dockerComposeVersionCmd := icmd.Command("docker", "compose", "version")
 
-	_, err := shell.RunCommandAndGetOutputE(t, checkDockerComposeVersionCmd)
+	result := icmd.RunCmd(dockerComposeVersionCmd)
 
-	if err != nil {
+	if result.ExitCode == 0 {
 		cmd = shell.Command{
-			Command:    "docker-compose",
-			Args:       append([]string{"--project-name", strings.ToLower(t.Name())}, args...),
+			Command:    "docker",
+			Args:       append([]string{"compose", "--project-name", strings.ToLower(t.Name())}, args...),
 			WorkingDir: options.WorkingDir,
 			Env:        options.EnvVars,
 			Logger:     options.Logger,
