c4fdf05413b50bc3efb28dd70f1abb4bc30825b6
Jacob Crawford <jacobcrawford1@hotmail.com>
2023-01-31 21:01:49 +0100

    Build tags differentiate K8s tests and non-k8s test

--- test/kubernetes_basic_example_logs_test.go
+++ test/kubernetes_basic_example_logs_test.go
@@ -1,3 +1,6 @@
+//go:build kubeall || kubernetes
+// +build kubeall kubernetes
+
 // NOTE: we have build tags to differentiate kubernetes tests from non-kubernetes tests. This is done because minikube
 // is heavy and can interfere with docker related tests in terratest. Specifically, many of the tests start to fail with
 // `connection refused` errors from `minikube`. To avoid overloading the system, we run the kubernetes tests and helm
