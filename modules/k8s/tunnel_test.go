5d3c5988c0ef53614ed3e163eed281d15e16eac0
Marcell Martini <marcellmartini@gmail.com>
2023-04-25 09:32:38 -0400

    Add support port forwarding for deployment resources.

--- modules/k8s/tunnel_test.go
+++ modules/k8s/tunnel_test.go
@@ -49,6 +49,35 @@ func TestTunnelOpensAPortForwardTunnelToPod(t *testing.T) {
 	)
 }
 
+func TestTunnelOpensAPortForwardTunnelToDeployment(t *testing.T) {
+	t.Parallel()
+
+	uniqueID := strings.ToLower(random.UniqueId())
+	options := NewKubectlOptions("", "", uniqueID)
+	configData := fmt.Sprintf(ExampleDeploymentYAMLTemplate, uniqueID)
+	KubectlApplyFromString(t, options, configData)
+	defer KubectlDeleteFromString(t, options, configData)
+	WaitUntilDeploymentAvailable(t, options, "nginx-deployment", 60, 1*time.Second)
+
+	// Open a tunnel to pod from any available port locally
+	tunnel := NewTunnel(options, ResourceTypeDeployment, "nginx-deployment", 0, 80)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	// Setup a TLS configuration to submit with the helper, a blank struct is acceptable
+	tlsConfig := tls.Config{}
+
+	// Try to access the nginx service on the local port, retrying until we get a good response for up to 5 minutes
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		fmt.Sprintf("http://%s", tunnel.Endpoint()),
+		&tlsConfig,
+		60,
+		5*time.Second,
+		verifyNginxWelcomePage,
+	)
+}
+
 func TestTunnelOpensAPortForwardTunnelToService(t *testing.T) {
 	t.Parallel()
 
